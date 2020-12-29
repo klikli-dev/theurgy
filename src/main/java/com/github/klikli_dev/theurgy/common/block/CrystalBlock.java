b3b7c5318c9e33ded21064361ae4055fdbb97dc8
klikli-dev <klikli@outlook.at>
2020-12-29 14:00:46 +0100

    Add waterlogging to crystal

--- src/main/java/com/github/klikli_dev/theurgy/common/block/CrystalBlock.java
+++ src/main/java/com/github/klikli_dev/theurgy/common/block/CrystalBlock.java
@@ -24,25 +24,30 @@ package com.github.klikli_dev.theurgy.common.block;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.DirectionalBlock;
+import net.minecraft.block.*;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 
 import java.util.Map;
 
-public class CrystalBlock extends DirectionalBlock {
+public class CrystalBlock extends DirectionalBlock implements IWaterLoggable {
+
+    //region Fields
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
     private static final Map<Direction, VoxelShape> SHAPES = Maps.newEnumMap(
             ImmutableMap.<Direction, VoxelShape>builder()
                     .put(Direction.EAST, Block.makeCuboidShape(4, 4, 4, 12, 12, 12))  //TODO: Find correct rotation
@@ -52,25 +57,53 @@ public class CrystalBlock extends DirectionalBlock {
                     .put(Direction.UP, Block.makeCuboidShape(4, 0, 4, 12, 14, 12))
                     .put(Direction.DOWN, Block.makeCuboidShape(4, 2, 4, 12, 16, 12))
                     .build());
+    //endregion Fields
+
     //region Initialization
     public CrystalBlock(AbstractBlock.Properties builder) {
         super(builder);
-        this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.UP));
+        this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.UP).with(WATERLOGGED, false));
     }
     //endregion Initialization
 
     //region Overrides
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        Direction direction = context.getFace();
+        BlockState blockstate = context.getWorld().getBlockState(context.getPos().offset(direction.getOpposite()));
+        FluidState fluidstate = context.getWorld().getFluidState(context.getPos());
+
+        return (blockstate.isIn(this) && blockstate.get(FACING) == direction ? this.getDefaultState().with(FACING,
+                direction.getOpposite()) : this.getDefaultState().with(FACING, direction))
+                       .with(WATERLOGGED, fluidstate.isTagged(FluidTags.WATER) && fluidstate.getLevel() == 8);
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(FACING).add(WATERLOGGED);
+    }
+
+    @Override
+    public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn,
+                                          BlockPos currentPos, BlockPos facingPos) {
+        if (stateIn.get(WATERLOGGED)) {
+            worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
+        }
+
+        return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+    }
 
 
     @Override
-    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        return SHAPES.get(state.get(BlockStateProperties.FACING));
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
     }
 
     /**
      * Returns the blockstate with the given rotation from the passed blockstate. If inapplicable, returns the passed
      * blockstate.
      */
+    @Override
     public BlockState rotate(BlockState state, Rotation rot) {
         return state.with(FACING, rot.rotate(state.get(FACING)));
     }
@@ -79,19 +112,14 @@ public class CrystalBlock extends DirectionalBlock {
      * Returns the blockstate with the given mirror of the passed blockstate. If inapplicable, returns the passed
      * blockstate.
      */
+    @Override
     public BlockState mirror(BlockState state, Mirror mirrorIn) {
         return state.with(FACING, mirrorIn.mirror(state.get(FACING)));
     }
 
-    public BlockState getStateForPlacement(BlockItemUseContext context) {
-        Direction direction = context.getFace();
-        BlockState blockstate = context.getWorld().getBlockState(context.getPos().offset(direction.getOpposite()));
-        return blockstate.isIn(this) && blockstate.get(FACING) == direction ? this.getDefaultState().with(FACING,
-                direction.getOpposite()) : this.getDefaultState().with(FACING, direction);
-    }
-
-    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPES.get(state.get(BlockStateProperties.FACING));
     }
     //endregion Overrides
 }

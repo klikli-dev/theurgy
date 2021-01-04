b4d5affe543a0f087d7d3211baa4bef9403fdf61
klikli-dev <klikli@outlook.at>
2021-01-04 19:17:56 +0100

    Create specific entities for aether and essentia

--- src/main/java/com/github/klikli_dev/theurgy/common/entity/GlowingBallEntity.java
+++ src/main/java/com/github/klikli_dev/theurgy/common/entity/GlowingBallEntity.java
@@ -46,10 +46,6 @@ public abstract class GlowingBallEntity extends Entity implements IEntityAdditio
     //region Initialization
     public GlowingBallEntity(EntityType<?> entityTypeIn, World worldIn) {
         super(entityTypeIn, worldIn);
-        //TODO: remove debug code
-        //      should instead use additional spawn data to transfer the color once packed?
-        //      or better: fixed color for aether + enum that returns color for essentia
-        this.color = new Vector3f(60.0f/255.0f, 1.0f, 16.0f/255.0f);
     }
 
     @Override
@@ -69,6 +65,7 @@ public abstract class GlowingBallEntity extends Entity implements IEntityAdditio
 
 
     //region Overrides
+
 
     @Override
     public void tick() {

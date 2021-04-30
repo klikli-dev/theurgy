350b690b95e2962f84d6f98115d1fb02a537dac2
klikli-dev <klikli@outlook.at>
2021-04-30 13:37:07 +0200

    Delete old state and setup empty kotlin for forge

---
+++ src/main/kotlin/com/klikli_dev/theurgy/Theurgy.kt
@@ -0,0 +1,50 @@
+/*
+ * MIT License
+ *
+ * Copyright 2021 klikli-dev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.klikli_dev.theurgy
+
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
+import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent
+import org.apache.logging.log4j.LogManager
+import thedarkcolour.kotlinforforge.forge.MOD_BUS
+
+@Mod(Theurgy.MOD_ID)
+object Theurgy {
+
+    const val MOD_ID: String = "theurgy"
+
+    val LOGGER = LogManager.getLogger(MOD_ID)
+
+    init {
+        //register event buses
+        MOD_BUS.addListener(Theurgy::commonSetup)
+        MOD_BUS.addListener(Theurgy::serverSetup)
+    }
+
+    private fun commonSetup(@Suppress("UNUSED_PARAMETER") event: FMLCommonSetupEvent) {
+        LOGGER.info("Common setup complete.")
+    }
+
+    private fun serverSetup(@Suppress("UNUSED_PARAMETER") event: FMLDedicatedServerSetupEvent) {
+        LOGGER.info("Dedicated server setup complete.")
+    }
+}

544fa1a681f6ed2cb57fa45e5d93cddb59149a82
Kli Kli <klikli@outlook.at>
2021-10-16 10:15:34 +0200

    Replace usage of OnlyIn with clean side separation

--- src/main/java/com/klikli_dev/theurgy/network/ClientMessageHandler.java
+++ src/main/java/com/klikli_dev/theurgy/network/ClientMessageHandler.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright 2020 klikli-dev
+ * Copyright 2021 klikli-dev
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction, including
@@ -23,33 +23,14 @@
 package com.klikli_dev.theurgy.network;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.server.MinecraftServer;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fmllegacy.network.NetworkEvent;
 
 import java.util.function.Supplier;
 
-public class MessageHandler {
+public class ClientMessageHandler {
 
-    public static <T extends IMessage> void handle(T message, Supplier<NetworkEvent.Context> ctx) {
-        if (ctx.get().getDirection().getReceptionSide() == LogicalSide.SERVER) {
-            ctx.get().enqueueWork(() -> {
-                handleServer(message, ctx);
-            });
-        } else {
-            ctx.get().enqueueWork(() -> {
-                //separate class to avoid loading client code on server.
-                //Using OnlyIn on a method in this class would work too, but is discouraged
-                ClientMessageHandler.handleClient(message, ctx);
-            });
-        }
-        ctx.get().setPacketHandled(true);
-    }
-
-    public static <T extends IMessage> void handleServer(T message, Supplier<NetworkEvent.Context> ctx) {
-        MinecraftServer server = ctx.get().getSender().level.getServer();
-        message.onServerReceived(server, ctx.get().getSender(), ctx.get());
+    public static <T extends IMessage> void handleClient(T message, Supplier<NetworkEvent.Context> ctx) {
+        Minecraft minecraft = Minecraft.getInstance();
+        message.onClientReceived(minecraft, minecraft.player, ctx.get());
     }
 }

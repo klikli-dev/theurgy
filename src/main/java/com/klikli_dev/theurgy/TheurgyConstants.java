7fa403af1cf5a87e25664426870822240c7ff8df
Kli Kli <klikli@outlook.at>
2022-11-26 15:46:21 +0100

    Take sulfur render item from nbt

---
+++ src/main/java/com/klikli_dev/theurgy/TheurgyConstants.java
@@ -0,0 +1,29 @@
+/*
+ * SPDX-FileCopyrightText: 2022 klikli-dev
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.klikli_dev.theurgy;
+
+public class TheurgyConstants {
+
+    public static class Nbt {
+
+        protected static final String PREFIX = Theurgy.MODID + ":";
+
+        /**
+         * Used to store item id of the item the sulfur was created from.
+         * Should be a ResourceLocation string.
+         * Sample Command: /give @s theurgy:alchemical_sulfur{"theurgy:sulfur.source.id":"minecraft:oak_log"}
+         */
+        public static final String SULFUR_SOURCE_ID = PREFIX + "sulfur.source.id";
+
+        /**
+         * Used to store stack NBT of the stack the sulfur was created from.
+         * Should be a CompoundTag.
+         */
+        public static final String SULFUR_SOURCE_NBT = PREFIX + "sulfur.source.nbt";
+    }
+
+}
--- src/main/java/com/klikli_dev/theurgy/client/render/SulfurBEWLR.java
+++ src/main/java/com/klikli_dev/theurgy/client/render/SulfurBEWLR.java
@@ -6,18 +6,22 @@
 
 package com.klikli_dev.theurgy.client.render;
 
+import com.klikli_dev.theurgy.TheurgyConstants;
 import com.klikli_dev.theurgy.registry.ItemRegistry;
 import com.mojang.blaze3d.platform.Lighting;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class SulfurBEWLR extends BlockEntityWithoutLevelRenderer {
 
@@ -38,10 +42,10 @@ public class SulfurBEWLR extends BlockEntityWithoutLevelRenderer {
     }
 
     @Override
-    public void renderByItem(ItemStack pStack, ItemTransforms.TransformType pTransformType, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight, int pPackedOverlay) {
+    public void renderByItem(ItemStack sulfurStack, ItemTransforms.TransformType pTransformType, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight, int pPackedOverlay) {
 
         pPoseStack.popPose();
-        pPoseStack.pushPose();
+        pPoseStack.pushPose(); //reset pose that we get from the item renderer, it moves by half a block which we don't want
 
         ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
 
@@ -55,15 +59,18 @@ public class SulfurBEWLR extends BlockEntityWithoutLevelRenderer {
 
         //note: if we reset to 3d item light here it ignores it above and renders dark .. idk why
 
-        this.renderLabel(pStack, pTransformType, pPoseStack, pBuffer, pPackedLight, pPackedOverlay);
-        this.renderContainedItem(pStack, pTransformType, pPoseStack, pBuffer, pPackedLight, pPackedOverlay);
+        this.renderLabel(sulfurStack, pTransformType, pPoseStack, pBuffer, pPackedLight, pPackedOverlay);
+        this.renderContainedItem(sulfurStack, pTransformType, pPoseStack, pBuffer, pPackedLight, pPackedOverlay);
     }
 
-    public void renderLabel(ItemStack pStack, ItemTransforms.TransformType pTransformType, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight, int pPackedOverlay) {
+    public void renderLabel(ItemStack sulfurStack, ItemTransforms.TransformType pTransformType, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight, int pPackedOverlay) {
 
         ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
 
         BakedModel labelModel = itemRenderer.getModel(labelStack, null, null, 0);
+
+        //now apply the transform to the label to make it look right in-world -> because below we render with gui transform which would mess it up
+        labelModel = labelModel.applyTransform(pTransformType, pPoseStack, isLeftHand(pTransformType));
 
         pPoseStack.pushPose();
 
@@ -82,28 +89,37 @@ public class SulfurBEWLR extends BlockEntityWithoutLevelRenderer {
 
     }
 
-    public void renderContainedItem(ItemStack pStack, ItemTransforms.TransformType pTransformType, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight, int pPackedOverlay) {
+    public void renderContainedItem(ItemStack sulfurStack, ItemTransforms.TransformType pTransformType, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight, int pPackedOverlay) {
 
-        //TODO: Get item from nbt
         ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
-        var containedStack = new ItemStack(Items.COPPER_ORE);
-        BakedModel containedModel = itemRenderer.getModel(containedStack, null, null, 0);
 
-        pPoseStack.pushPose();
+        if(sulfurStack.hasTag() && sulfurStack.getTag().contains(TheurgyConstants.Nbt.SULFUR_SOURCE_ID)){
+            var itemId = new ResourceLocation(sulfurStack.getTag().getString(TheurgyConstants.Nbt.SULFUR_SOURCE_ID));
+            var containedStack = new ItemStack(ForgeRegistries.ITEMS.getValue(itemId));
 
-        float pixel = 1f / 16f;
-        pPoseStack.translate(0, 0, pixel); //move it before item
+            if(sulfurStack.getTag().contains(TheurgyConstants.Nbt.SULFUR_SOURCE_NBT))
+                containedStack.setTag(sulfurStack.getTag().getCompound(TheurgyConstants.Nbt.SULFUR_SOURCE_NBT));
 
-        var scale = 0.45f;
-        pPoseStack.scale(scale, scale, scale);
-        pPoseStack.translate(0, -pixel * 3.5, 0); //position it on the item -> center
-        pPoseStack.scale(0.74F, 0.74F, 0.01F); //flatten item
+            BakedModel containedModel = itemRenderer.getModel(containedStack, null, null, 0);
 
-        Lighting.setupForFlatItems(); //always render "labeled" item flat
-        itemRenderer.render(containedStack, ItemTransforms.TransformType.GUI, isLeftHand(pTransformType), pPoseStack, pBuffer, pPackedLight, pPackedOverlay, containedModel);
-        //note: if we reset to 3d item light here it ignores it above and renders dark .. idk why
+            //not sure why we don't have to apply transform here. Somehow label carries over, because if we do not call label our rendering is off
+            //containedModel = containedModel.applyTransform(pTransformType, pPoseStack, isLeftHand(pTransformType));
 
-        pPoseStack.popPose();
+            pPoseStack.pushPose();
 
+            float pixel = 1f / 16f;
+            pPoseStack.translate(0, 0, pixel * 0.6); //move it before label
+
+            var scale = 0.45f;
+            pPoseStack.scale(scale, scale, scale);
+            pPoseStack.translate(0, -pixel * 3.5, 0); //position it on the item -> center
+            pPoseStack.scale(0.74F, 0.74F, 0.01F); //flatten item
+
+            Lighting.setupForFlatItems(); //always render "labeled" item flat
+            itemRenderer.render(containedStack, ItemTransforms.TransformType.GUI, isLeftHand(pTransformType), pPoseStack, pBuffer, pPackedLight, pPackedOverlay, containedModel);
+            //note: if we reset to 3d item light here it ignores it above and renders dark .. idk why
+
+            pPoseStack.popPose();
+        }
     }
 }

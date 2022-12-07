76f954a644780dbab9684406f971b6b5e17f255a
Kli Kli <klikli@outlook.at>
2022-12-07 15:52:11 +0100

    fix outdated nav url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/',
               },
             ],
           },

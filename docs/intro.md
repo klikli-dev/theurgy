1fab0a550bc8ec70fb6878581d8326678b5e2be6
Kli Kli <klikli@outlook.at>
2022-12-07 15:49:08 +0100

    Set up docusaurus

---
+++ docs/intro.md
@@ -0,0 +1,18 @@
+---
+sidebar_position: 1
+slug: / #set this as home page
+---
+
+# Introduction
+
+Theurgy is an open-source magic mod built around classical alchemy to replicate and transmutate matter, and create magical artifacts.
+
+This documentation site aims to help Modpack makers to understand how to extend and customize the mod. 
+
+Player documentation is found in-game in the guidebook "Hermetica".
+
+:::tip
+
+If you run into any issues or have any questions, join our Discord: **https://invite.gg/klikli**
+
+:::
---
+++ docusaurus.config.js
@@ -0,0 +1,110 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Theurgy',
+  tagline: 'An open-source magic mod built around classical alchemy to replicate and transmutate matter, and create magical artifacts.',
+  url: 'https://klikli-dev.github.io',
+  baseUrl: '/theurgy',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'klikli-dev', // Usually your GitHub org/user name.
+  projectName: 'theurgy', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/klikli-dev/theurgy/tree/documentation'
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Theurgy',
+        logo: {
+          alt: 'Theurgy Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            href: 'https://github.com/klikli-dev/theurgy',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://invite.gg/klikli',
+              }
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+      
+              {
+                label: 'GitHub',
+                href: 'https://github.com/klikli-dev/theurgy',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} klikli-dev. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;

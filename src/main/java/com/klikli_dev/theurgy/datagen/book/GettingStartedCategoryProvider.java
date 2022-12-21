92ef8afbb4dbd4dadc94a92482d502b7ba612292
Kli Kli <klikli@outlook.at>
2022-12-21 18:32:02 +0100

    feat: add about mod entry to getting started

--- src/main/java/com/klikli_dev/theurgy/datagen/book/GettingStartedCategoryProvider.java
+++ src/main/java/com/klikli_dev/theurgy/datagen/book/GettingStartedCategoryProvider.java
@@ -11,10 +11,13 @@ import com.klikli_dev.modonomicon.api.datagen.BookLangHelper;
 import com.klikli_dev.modonomicon.api.datagen.EntryLocationHelper;
 import com.klikli_dev.modonomicon.api.datagen.book.BookCategoryModel;
 import com.klikli_dev.modonomicon.api.datagen.book.BookEntryModel;
+import com.klikli_dev.modonomicon.api.datagen.book.BookEntryParentModel;
 import com.klikli_dev.modonomicon.api.datagen.book.page.BookTextPageModel;
 import com.klikli_dev.theurgy.Theurgy;
 import com.klikli_dev.theurgy.registry.ItemRegistry;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.common.data.LanguageProvider;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class GettingStartedCategoryProvider {
 
@@ -30,26 +33,29 @@ public class GettingStartedCategoryProvider {
                 "__________________________________",
                 "__________i_______________________",
                 "__________________________________",
-                "__________________________________",
+                "__________a_______________________",
                 "__________________________________",
                 "__________________________________"
         );
 
         var introEntry = this.makeIntroEntry(helper, lang, entryHelper, 'i');
-
+        var aboutModEntry = this.makeAboutModEntry(helper, lang, entryHelper, 'a');
+        //make about mod entry depend on intro
+        aboutModEntry.withParent(introEntry);
 
         return BookCategoryModel.builder()
                 .withId(Theurgy.loc((helper.category)))
                 .withName(helper.categoryName())
                 .withIcon(ItemRegistry.THE_HERMETICA_ICON.getId().toString())
                 .withEntries(
-                        introEntry.build()
+                        introEntry.build(),
+                        aboutModEntry.build()
                 );
     }
 
     private BookEntryModel.Builder makeIntroEntry(BookLangHelper helper, LanguageProvider lang, EntryLocationHelper entryHelper, char icon) {
         helper.entry("intro");
-        lang.add(helper.entryName(), "About");
+        lang.add(helper.entryName(), "About this Work");
         lang.add(helper.entryDescription(), "About using The Hermetica");
 
         helper.page("intro");
@@ -57,10 +63,10 @@ public class GettingStartedCategoryProvider {
                 .withTitle(helper.pageTitle())
                 .withText(helper.pageText())
                 .build();
-        lang.add(helper.pageTitle(), "About");
+        lang.add(helper.pageTitle(), "About this Work");
         lang.add(helper.pageText(),
                 """
-                        The following pages will lead the novice alchemist on their journey through the noble art of the transformation of matter and mind. This humble author will share his experiences, thoughts and research notes to guide the valued reader in as safe a manner as the subject matter allows.
+                        The following pages will lead the novice alchemist on their journey through the noble art of the transformation of matter and mind. This humble author will share their experiences, thoughts and research notes to guide the valued reader in as safe a manner as the subject matter allows.
                         """);
 
         helper.page("help");
@@ -91,4 +97,69 @@ public class GettingStartedCategoryProvider {
                 );
     }
 
+    private BookEntryModel.Builder makeAboutModEntry(BookLangHelper helper, LanguageProvider lang, EntryLocationHelper entryHelper, char icon) {
+        helper.entry("about_mod");
+        lang.add(helper.entryName(), "The Art of Alchemy");
+        lang.add(helper.entryDescription(), "About this Mod");
+
+        helper.page("about");
+        var about = BookTextPageModel.builder()
+                .withTitle(helper.pageTitle())
+                .withText(helper.pageText())
+                .build();
+        lang.add(helper.pageTitle(), "The Art of Alchemy");
+        lang.add(helper.pageText(),
+                """
+                   Welcome, dear reader, to Theurgy, a mod that explores the ancient and revered art of classical alchemy. As you embark on your journey through the noble art of transformation, you will be equipped with divination rods to make finding resources in the world easier.
+                   """);
+        helper.page("about2");
+        var about2 = BookTextPageModel.builder()
+                .withText(helper.pageText())
+                .build();
+        lang.add(helper.pageText(),
+                """
+                   Through diligent study and practice, you will then learn to use alchemical devices to refine, replicate, and transform resources into new and useful materials. Along the way, you will have the opportunity to craft alchemical devices and equipment to aid you in your endeavors.
+                   """);
+
+        helper.page("features");
+        var features = BookTextPageModel.builder()
+                .withTitle(helper.pageTitle())
+                .withText(helper.pageText())
+                .build();
+        lang.add(helper.pageTitle(), "Features");
+        lang.add(helper.pageText(),
+                """
+                        - Divination rods to find ores
+                        - Future: Ore refining (= more ingots per ore)
+                        - Future: Item replication (create duplicates of items you have)
+                        - Future: Item transformation (create new items from other items)
+                        """);
+
+        helper.page("features2");
+        var features2 = BookTextPageModel.builder()
+                .withTitle(helper.pageTitle())
+                .withText(helper.pageText())
+                .build();
+        lang.add(helper.pageTitle(), "More Features");
+        lang.add(helper.pageText(),
+                """
+                        - Future: Weapons and Equipment
+                        - Future: Devices to assist in common tasks
+                        """);
+
+
+        return BookEntryModel.builder()
+                .withId(Theurgy.loc(helper.category + "/" + helper.entry))
+                .withName(helper.entryName())
+                .withDescription(helper.entryDescription())
+                .withIcon(ForgeRegistries.ITEMS.getKey(Items.NETHER_STAR).toString())
+                .withLocation(entryHelper.get(icon))
+                .withEntryBackground(0, 0)
+                .withPages(
+                        about,
+                        about2,
+                        features,
+                        features2
+                );
+    }
 }

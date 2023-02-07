8793d5ad4d484cdfc419d243f543ba6ab2ea8e2e
Kli Kli <klikli@outlook.at>
2023-02-07 20:15:02 +0100

    chore: remove no longer existing menu

--- src/main/java/com/klikli_dev/theurgy/registry/MenuTypeRegistry.java
+++ src/main/java/com/klikli_dev/theurgy/registry/MenuTypeRegistry.java
@@ -7,18 +7,11 @@
 package com.klikli_dev.theurgy.registry;
 
 import com.klikli_dev.theurgy.Theurgy;
-import com.klikli_dev.theurgy.menu.CalcinationOvenMenu;
-import com.klikli_dev.theurgy.recipe.CalcinationRecipe;
 import net.minecraft.world.inventory.MenuType;
-import net.minecraft.world.item.crafting.Recipe;
-import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 public class MenuTypeRegistry {
     public static final DeferredRegister<MenuType<?>> MENU_TYPES = DeferredRegister.create(
             ForgeRegistries.MENU_TYPES, Theurgy.MODID);
-    public static final RegistryObject<MenuType<CalcinationOvenMenu>> CALCINATION_OVEN = MENU_TYPES.register(
-            "calcination_oven", () -> new MenuType<>(CalcinationOvenMenu::new));
 }

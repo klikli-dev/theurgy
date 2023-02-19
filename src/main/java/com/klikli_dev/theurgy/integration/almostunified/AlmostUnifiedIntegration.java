af4f12349bd6339ec63e7e73ccb9e5a1ca81d4c1
Kli Kli <klikli@outlook.at>
2023-02-19 20:24:44 +0100

    feat: add almostunified integration

---
+++ src/main/java/com/klikli_dev/theurgy/integration/almostunified/AlmostUnifiedIntegration.java
@@ -0,0 +1,35 @@
+/*
+ * SPDX-FileCopyrightText: 2023 klikli-dev
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.klikli_dev.theurgy.integration.almostunified;
+
+import com.almostreliable.unified.api.AlmostUnifiedLookup;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.fml.ModList;
+import org.jetbrains.annotations.Nullable;
+
+public class AlmostUnifiedIntegration {
+    public static boolean isLoaded() {
+        return ModList.get().isLoaded("almostunified");
+    }
+
+    @Nullable
+    public static Item getPreferredItemForTag(TagKey<Item> tag) {
+        if (isLoaded()) {
+            return AlmostUnifiedHelper.getPreferredItemForTag(tag);
+        }
+
+        return null;
+    }
+
+    public static class AlmostUnifiedHelper {
+        @Nullable
+        public static Item getPreferredItemForTag(TagKey<Item> tag) {
+            return AlmostUnifiedLookup.INSTANCE.getPreferredItemForTag(tag);
+        }
+    }
+}
---
+++ src/main/java/com/klikli_dev/theurgy/util/ItemUtil.java
@@ -0,0 +1,37 @@
+/*
+ * SPDX-FileCopyrightText: 2023 klikli-dev
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.klikli_dev.theurgy.util;
+
+import com.klikli_dev.theurgy.integration.almostunified.AlmostUnifiedIntegration;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.stream.Stream;
+
+public class ItemUtil {
+
+    @Nullable
+    public static Item getItemForTag(TagKey<Item> tag) {
+        var item = AlmostUnifiedIntegration.getPreferredItemForTag(tag);
+
+        return item != null ? item : BuiltInRegistries.ITEM.getTag(tag)
+                .map(HolderSet.ListBacked::stream)
+                .flatMap(Stream::findFirst)
+                .map(Holder::value)
+                .orElse(null);
+    }
+
+    public static ItemStack getItemStackForTag(TagKey<Item> tag) {
+        var item = getItemForTag(tag);
+        return item != null ? new ItemStack(item) : ItemStack.EMPTY;
+    }
+}

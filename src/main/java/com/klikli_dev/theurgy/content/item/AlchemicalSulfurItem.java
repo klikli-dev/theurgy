d510d54d8dd6dbfe9c388f8f6f11f685e8be3a33
Kli Kli <klikli@outlook.at>
2023-02-19 20:25:31 +0100

    feat: add sulfur source support for tags

--- src/main/java/com/klikli_dev/theurgy/content/item/AlchemicalSulfurItem.java
+++ src/main/java/com/klikli_dev/theurgy/content/item/AlchemicalSulfurItem.java
@@ -9,14 +9,17 @@ package com.klikli_dev.theurgy.content.item;
 import com.google.common.collect.ImmutableList;
 import com.klikli_dev.theurgy.TheurgyConstants;
 import com.klikli_dev.theurgy.content.renderer.SulfurBEWLR;
+import com.klikli_dev.theurgy.util.ItemUtil;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.ComponentUtils;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -33,8 +36,18 @@ public class AlchemicalSulfurItem extends Item {
 
     public static ItemStack getSourceStack(ItemStack sulfurStack) {
         if (sulfurStack.hasTag() && sulfurStack.getTag().contains(TheurgyConstants.Nbt.SULFUR_SOURCE_ID)) {
-            var itemId = new ResourceLocation(sulfurStack.getTag().getString(TheurgyConstants.Nbt.SULFUR_SOURCE_ID));
-            var sourceStack = new ItemStack(ForgeRegistries.ITEMS.getValue(itemId));
+
+            var itemSourceId = sulfurStack.getTag().getString(TheurgyConstants.Nbt.SULFUR_SOURCE_ID);
+            ItemStack sourceStack;
+
+            if(itemSourceId.startsWith("#")){
+                var tagId = new ResourceLocation(itemSourceId.substring(1));
+                var tag = TagKey.create(Registries.ITEM, tagId);
+                sourceStack = ItemUtil.getItemStackForTag(tag);
+            } else {
+                var itemId = new ResourceLocation(sulfurStack.getTag().getString(TheurgyConstants.Nbt.SULFUR_SOURCE_ID));
+                sourceStack = new ItemStack(ForgeRegistries.ITEMS.getValue(itemId));
+            }
 
             if (sulfurStack.getTag().contains(TheurgyConstants.Nbt.SULFUR_SOURCE_NBT))
                 sourceStack.setTag(sulfurStack.getTag().getCompound(TheurgyConstants.Nbt.SULFUR_SOURCE_NBT));
@@ -89,6 +102,8 @@ public class AlchemicalSulfurItem extends Item {
             var level = Minecraft.getInstance().level;
             if (level != null) {
                 var recipeManager = level.getRecipeManager();
+                //TODO: we probably want to limit it to liquefaction recipes for performance reasons
+                //TODO: most likely we also want to do it in one go for all sulfur items
                 recipeManager.getRecipes().forEach((recipe) -> {
                     if (recipe.getResultItem() != null && recipe.getResultItem().getItem() == item) {
                         output.accept(recipe.getResultItem().copy());

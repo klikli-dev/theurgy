4181166b62042eee950f4df4f8b072fa99bd4b2d
Kli Kli <klikli@outlook.at>
2023-02-22 17:43:01 +0100

    feat: add fluit input/output handling to cauldron block

--- src/main/java/com/klikli_dev/theurgy/content/block/liquefactioncauldron/LiquefactionCauldronBlock.java
+++ src/main/java/com/klikli_dev/theurgy/content/block/liquefactioncauldron/LiquefactionCauldronBlock.java
@@ -6,13 +6,13 @@
 
 package com.klikli_dev.theurgy.content.block.liquefactioncauldron;
 
-import com.klikli_dev.theurgy.content.block.calcinationoven.CalcinationOvenBlockEntity;
 import com.klikli_dev.theurgy.registry.BlockEntityRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
@@ -28,6 +28,7 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.items.wrapper.RecipeWrapper;
 import org.jetbrains.annotations.Nullable;
 
@@ -61,9 +62,66 @@ public class LiquefactionCauldronBlock extends Block implements EntityBlock {
 
     @Override
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
-        //TODO: implement use
-        //  add/remove input
-        //  add/remove solvent
+        if (pLevel.isClientSide()) {
+            return InteractionResult.SUCCESS;
+        }
+
+        if (pLevel.getBlockEntity(pPos) instanceof LiquefactionCauldronBlockEntity blockEntity) {
+
+            ItemStack stackInHand = pPlayer.getItemInHand(pHand);
+
+            //first attempt fluid transfer
+            var itemFluidHandlerCap = FluidUtil.getFluidHandler(stackInHand);
+            if (itemFluidHandlerCap.isPresent()) {
+                var itemFluidHandler = itemFluidHandlerCap.orElse(null); //ok here because we checked for presence
+
+                //first we try to insert
+                var transferredFluid = FluidUtil.tryFluidTransfer(blockEntity.solventTank, itemFluidHandler,
+                        Integer.MAX_VALUE, true);
+                if (!transferredFluid.isEmpty()) {
+                    pPlayer.setItemInHand(pHand, itemFluidHandler.getContainer()); //always set to container to handle e.g. empty bucket correctly
+                    blockEntity.setChanged();
+                    return InteractionResult.SUCCESS;
+                }
+
+                //if that fails, try to extract
+                transferredFluid = FluidUtil.tryFluidTransfer(itemFluidHandler, blockEntity.solventTank,
+                        Integer.MAX_VALUE, true);
+                if (!transferredFluid.isEmpty()) {
+                    pPlayer.setItemInHand(pHand, itemFluidHandler.getContainer());
+                    blockEntity.setChanged();
+                    return InteractionResult.SUCCESS;
+                }
+            }
+
+            //only if that fails try item transfer
+            var outputStack = blockEntity.outputInventory.getStackInSlot(0);
+            var inputStack = blockEntity.inputInventory.getStackInSlot(0);
+            if (stackInHand.isEmpty()) {
+                if (!outputStack.isEmpty()) {
+                    //with empty hand first try take output
+                    pPlayer.getInventory().placeItemBackInInventory(outputStack);
+                    blockEntity.outputInventory.setStackInSlot(0, ItemStack.EMPTY);
+                    blockEntity.setChanged();
+                    return InteractionResult.SUCCESS;
+                } else if (!inputStack.isEmpty()) {
+                    //if no output, try take input
+                    pPlayer.getInventory().placeItemBackInInventory(inputStack);
+                    blockEntity.inputInventory.setStackInSlot(0, ItemStack.EMPTY);
+                    blockEntity.setChanged();
+                    return InteractionResult.SUCCESS;
+                }
+            } else {
+                //if we have an item in hand, try to insert
+                var remainder = blockEntity.inputInventory.insertItem(0, stackInHand, false);
+                pPlayer.setItemInHand(pHand, remainder);
+                if (remainder.getCount() != stackInHand.getCount()) {
+                    blockEntity.setChanged();
+                    return InteractionResult.SUCCESS;
+                }
+                return InteractionResult.PASS;
+            }
+        }
 
         return InteractionResult.PASS;
     }
@@ -71,8 +129,8 @@ public class LiquefactionCauldronBlock extends Block implements EntityBlock {
     @Override
     public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
         if (!pState.is(pNewState.getBlock())) {
-            if (pLevel.getBlockEntity(pPos) instanceof CalcinationOvenBlockEntity calcinationOven) {
-                Containers.dropContents(pLevel, pPos, new RecipeWrapper(calcinationOven.inventory));
+            if (pLevel.getBlockEntity(pPos) instanceof LiquefactionCauldronBlockEntity blockEntity) {
+                Containers.dropContents(pLevel, pPos, new RecipeWrapper(blockEntity.inventory));
             }
         }
         super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);

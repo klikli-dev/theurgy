aeec1104f5120c36189ce59a896b8d4987dd99ed
Kli Kli <klikli@outlook.at>
2023-02-23 17:39:27 +0100

    feat: add block interaction entry (#80)
    Closes #75

--- src/main/java/com/klikli_dev/theurgy/datagen/book/GettingStartedCategoryProvider.java
+++ src/main/java/com/klikli_dev/theurgy/datagen/book/GettingStartedCategoryProvider.java
@@ -15,6 +15,7 @@ import com.klikli_dev.modonomicon.api.datagen.book.page.BookCraftingRecipePageMo
 import com.klikli_dev.modonomicon.api.datagen.book.page.BookSpotlightPageModel;
 import com.klikli_dev.modonomicon.api.datagen.book.page.BookTextPageModel;
 import com.klikli_dev.theurgy.Theurgy;
+import com.klikli_dev.theurgy.registry.BlockRegistry;
 import com.klikli_dev.theurgy.registry.ItemRegistry;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -30,9 +31,9 @@ public class GettingStartedCategoryProvider {
         entryHelper.setMap(
                 "__________________________________",
                 "__________________________________",
+                "____________u_____________________",
                 "__________________________________",
-                "__________________________________",
-                "__________i_d_____________________",
+                "__________i___d___________________",
                 "__________________________________",
                 "__________a_______________________",
                 "__________________________________",
@@ -46,6 +47,9 @@ public class GettingStartedCategoryProvider {
         var divinationRodEntry = this.makeDivinationRodEntry(helper, lang, entryHelper, 'd');
         divinationRodEntry.withParent(introEntry);
 
+        var apparatusHowToEntry = this.makeApparatusHowToEntry(helper, lang, entryHelper, 'u');
+        apparatusHowToEntry.withParent(introEntry);
+
         //TODO: higer tier div rod entries explaining how they work
 
         return BookCategoryModel.builder()
@@ -55,7 +59,8 @@ public class GettingStartedCategoryProvider {
                 .withEntries(
                         introEntry.build(),
                         aboutModEntry.build(),
-                        divinationRodEntry.build()
+                        divinationRodEntry.build(),
+                        apparatusHowToEntry.build()
                 );
     }
 
@@ -127,6 +132,15 @@ public class GettingStartedCategoryProvider {
                         Through diligent study and practice, you will then learn to use alchemical devices to refine, replicate, and transform resources into new and useful materials. Along the way, you will have the opportunity to craft alchemical devices and equipment to aid you in your endeavors.
                         """);
 
+        helper.page("about3");
+        var about3 = BookTextPageModel.builder()
+                .withText(helper.pageText())
+                .build();
+        lang.add(helper.pageText(),
+                """
+                        As a final note, alchemists are guided by reason and logic, not superstition or magic. Our experiments are based on careful observation, meticulous record-keeping, and rigorous testing. We do not claim to possess supernatural powers, but rather seek to harness the natural forces of the world around us to achieve our goals.
+                        """);
+
         helper.page("features");
         var features = BookTextPageModel.builder()
                 .withTitle(helper.pageTitle())
@@ -164,6 +178,7 @@ public class GettingStartedCategoryProvider {
                 .withPages(
                         about,
                         about2,
+                        about3,
                         features,
                         features2
                 );
@@ -201,8 +216,8 @@ public class GettingStartedCategoryProvider {
                 .build();
         lang.add(helper.pageText(),
                 """
-                     The most basic tier of divination rods, brittle and limited in it's application, but powerful nonetheless.
-                        """);
+                        The most basic tier of divination rods, brittle and limited in it's application, but powerful nonetheless.
+                           """);
 
         helper.page("supported_blocks");
         var supported_blocks = BookTextPageModel.builder()
@@ -243,5 +258,111 @@ public class GettingStartedCategoryProvider {
                         supported_blocks,
                         usage
                 );
+    }
+
+    private BookEntryModel.Builder makeApparatusHowToEntry(BookLangHelper helper, LanguageProvider lang, EntryLocationHelper entryHelper, char icon) {
+        helper.entry("apparatus_how_to");
+        lang.add(helper.entryName(), "Alchemical Apparatus");
+        lang.add(helper.entryDescription(), "How to interact with the tools of the trade");
+
+        helper.page("intro");
+        var intro = BookTextPageModel.builder()
+                .withTitle(helper.pageTitle())
+                .withText(helper.pageText())
+                .build();
+        lang.add(helper.pageTitle(), "Alchemical Apparatus");
+        lang.add(helper.pageText(),
+                """
+                        Alchemist use a variety of tools and devices to aid them in their work. These devices are collectively referred to as apparatus.
+                        \\
+                        \\
+                        It is important to understand that each apparatus should only have one specific function, such as generating heat or melting items.
+                                 """);
+
+        helper.page("intro2");
+        var intro2 = BookTextPageModel.builder()
+                .withText(helper.pageText())
+                .build();
+        lang.add(helper.pageText(),
+                """
+                        By adhering to this principle, we can create a modular system that allows for greater flexibility and efficiency in our work.
+                        \\
+                        \\
+                        Further, all apparatus follow a standardized interaction pattern that makes it easier to use them both for manual interactions and for automation.
+                                 """);
+
+        helper.page("manual_interaction");
+        var manualInteraction = BookTextPageModel.builder()
+                .withTitle(helper.pageTitle())
+                .withText(helper.pageText())
+                .build();
+        lang.add(helper.pageTitle(), "Manual Interaction");
+        lang.add(helper.pageText(),
+                """
+                        To interact with an apparatus, approach it and right-click on it.
+                        \\
+                        \\
+                        If you have an empty hand, the it will first try to take the contents of its output slot and place them in your inventory.
+                                    """);
+
+        helper.page("manual_interaction2");
+        var manualInteraction2 = BookTextPageModel.builder()
+                .withText(helper.pageText())
+                .build();
+        lang.add(helper.pageText(),
+                """
+                        If there are no output items, it will instead try to place the contents of its input slot into your inventory, effectively emptying it.
+                        \\
+                        \\
+                        If you have an item in your hand, the apparatus will automatically try to insert it into the input slot.
+                                    """);
+
+        helper.page("fluid_interaction");
+        var fluidInteraction = BookTextPageModel.builder()
+                .withTitle(helper.pageTitle())
+                .withText(helper.pageText())
+                .build();
+        lang.add(helper.pageTitle(), "Fluids");
+        lang.add(helper.pageText(),
+                """
+                        If you click on an apparatus with a filled fluid container in your hand, it will try to empty the container into the device.
+                        \\
+                        \\
+                        If you click on an apparatus with an empty fluid container in your hand, it will instead try to fill the container from the device.
+                                    """);
+
+        helper.page("automatic_interaction");
+        var automaticInteraction = BookTextPageModel.builder()
+                .withTitle(helper.pageTitle())
+                .withText(helper.pageText())
+                .build();
+
+        lang.add(helper.pageTitle(), "Automatic Interaction");
+        lang.add(helper.pageText(),
+                """
+                        Automatic interactions also use a standardized pattern.
+                        \\
+                        \\
+                        **Input** slots can be accessed from the **top**, while **output** slots are available at the **bottom**.\\
+                        \\
+                        A **combined inventory** can be found at the horizontal **sides**.
+                                  """);
+
+
+        return BookEntryModel.builder()
+                .withId(Theurgy.loc(helper.category + "/" + helper.entry))
+                .withName(helper.entryName())
+                .withDescription(helper.entryDescription())
+                .withIcon(BlockRegistry.CALCINATION_OVEN.get())
+                .withLocation(entryHelper.get(icon))
+                .withEntryBackground(0, 0)
+                .withPages(
+                        intro,
+                        intro2,
+                        manualInteraction,
+                        manualInteraction2,
+                        fluidInteraction,
+                        automaticInteraction
+                        );
     }
 }

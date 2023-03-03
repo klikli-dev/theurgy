785813e5f542707e1b53b074c0f02057c58e4134
Kli Kli <klikli@outlook.at>
2023-03-03 18:11:11 +0100

    chore: safeguard against mixing up sulfur and salt in incubation recipe gen

--- src/main/java/com/klikli_dev/theurgy/datagen/recipe/IncubationRecipeProvider.java
+++ src/main/java/com/klikli_dev/theurgy/datagen/recipe/IncubationRecipeProvider.java
@@ -8,6 +8,8 @@ package com.klikli_dev.theurgy.datagen.recipe;
 
 import com.google.gson.JsonObject;
 import com.klikli_dev.theurgy.Theurgy;
+import com.klikli_dev.theurgy.content.item.AlchemicalSaltItem;
+import com.klikli_dev.theurgy.content.item.AlchemicalSulfurItem;
 import com.klikli_dev.theurgy.content.recipe.IncubationRecipe;
 import com.klikli_dev.theurgy.registry.ItemRegistry;
 import com.klikli_dev.theurgy.registry.RecipeTypeRegistry;
@@ -34,23 +36,23 @@ public class IncubationRecipeProvider extends JsonRecipeProvider {
         this.makeRecipe(Items.WHEAT, ItemRegistry.MERCURY_SHARD.get(), SaltRegistry.CROPS.get(), SulfurRegistry.WHEAT.get());
     }
 
-    public void makeRecipe(TagKey<Item> result, Item mercury, Item salt, Item sulfur) {
+    public void makeRecipe(TagKey<Item> result, Item mercury, AlchemicalSaltItem salt, AlchemicalSulfurItem sulfur) {
         this.makeRecipe(result, 1, mercury, salt, sulfur, TIME);
     }
 
-    public void makeRecipe(TagKey<Item> result, Item mercury, Item salt, Item sulfur, int incubationTime) {
+    public void makeRecipe(TagKey<Item> result, Item mercury, AlchemicalSaltItem salt, AlchemicalSulfurItem sulfur, int incubationTime) {
         this.makeRecipe(result, 1, mercury, salt, sulfur, incubationTime);
     }
 
-    public void makeRecipe(TagKey<Item> result, int resultCount, Item mercury, Item salt, Item sulfur, int incubationTime) {
+    public void makeRecipe(TagKey<Item> result, int resultCount, Item mercury, AlchemicalSaltItem salt, AlchemicalSulfurItem sulfur, int incubationTime) {
         this.makeRecipe(this.name(result), result, resultCount, mercury, salt, sulfur, incubationTime);
     }
 
-    public void makeRecipe(String recipeName, TagKey<Item> result, Item mercury, Item salt, Item sulfur, int incubationTime) {
+    public void makeRecipe(String recipeName, TagKey<Item> result, Item mercury, AlchemicalSaltItem salt, AlchemicalSulfurItem sulfur, int incubationTime) {
         this.makeRecipe(recipeName, result, 1, mercury, salt, sulfur, incubationTime);
     }
 
-    public void makeRecipe(String recipeName, TagKey<Item> result, int resultCount, Item mercury, Item salt, Item sulfur, int incubationTime) {
+    public void makeRecipe(String recipeName, TagKey<Item> result, int resultCount, Item mercury, AlchemicalSaltItem salt, AlchemicalSulfurItem sulfur, int incubationTime) {
         this.recipeConsumer.accept(
                 this.modLoc(recipeName),
                 this.makeRecipeJson(
@@ -61,36 +63,24 @@ public class IncubationRecipeProvider extends JsonRecipeProvider {
 
     }
 
-    public void makeRecipe(Item result, Item mercury, Item salt, Item sulfur) {
+    public void makeRecipe(Item result, Item mercury, AlchemicalSaltItem salt, AlchemicalSulfurItem sulfur) {
         this.makeRecipe(result, 1, mercury, salt, sulfur, TIME);
     }
 
-    public void makeRecipe(Item result, Item mercury, Item salt, Item sulfur, int incubationTime) {
+    public void makeRecipe(Item result, Item mercury, AlchemicalSaltItem salt, AlchemicalSulfurItem sulfur, int incubationTime) {
         this.makeRecipe(result, 1, mercury, salt, sulfur, incubationTime);
     }
 
-    public void makeRecipe(Item result, int resultCount, Item mercury, Item salt, Item sulfur, int incubationTime) {
+    public void makeRecipe(Item result, int resultCount, Item mercury, AlchemicalSaltItem salt, AlchemicalSulfurItem sulfur, int incubationTime) {
         this.makeRecipe(this.name(result), result, resultCount, mercury, salt, sulfur, incubationTime);
     }
 
-    public void makeRecipe(String recipeName, Item result, Item mercury, Item salt, Item sulfur, int incubationTime) {
+    public void makeRecipe(String recipeName, Item result, Item mercury, AlchemicalSaltItem salt, AlchemicalSulfurItem sulfur, int incubationTime) {
         this.makeRecipe(recipeName, result, 1, mercury, salt, sulfur, incubationTime);
     }
 
-    public void makeRecipe(String recipeName, Item result, int resultCount, Item mercury, Item salt, Item sulfur, int incubationTime) {
+    public void makeRecipe(String recipeName, Item result, int resultCount, Item mercury, AlchemicalSaltItem salt, AlchemicalSulfurItem sulfur, int incubationTime) {
         this.recipeConsumer.accept(
-                this.modLoc(recipeName),
-                this.makeRecipeJson(
-                        this.makeItemIngredient(this.locFor(mercury)),
-                        this.makeItemIngredient(this.locFor(salt)),
-                        this.makeItemIngredient(this.locFor(sulfur)),
-                        this.makeItemResult(this.locFor(result), resultCount), incubationTime));
-
-    }
-
-    public void makeRecipe(String recipeName, Item result, int resultCount, Item mercury, Item salt, Item sulfur, int incubationTime, BiConsumer<ResourceLocation, JsonObject> recipeConsumer) {
-
-        recipeConsumer.accept(
                 this.modLoc(recipeName),
                 this.makeRecipeJson(
                         this.makeItemIngredient(this.locFor(mercury)),

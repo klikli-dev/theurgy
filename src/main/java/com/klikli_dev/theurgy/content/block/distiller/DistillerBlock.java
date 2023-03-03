4f917485f1821ec33a4b059ff4509c5c56aa4b18
Kli Kli <klikli@outlook.at>
2023-03-03 11:11:41 +0100

    chore: refactor interaction interfaces

--- src/main/java/com/klikli_dev/theurgy/content/block/distiller/DistillerBlock.java
+++ src/main/java/com/klikli_dev/theurgy/content/block/distiller/DistillerBlock.java
@@ -6,15 +6,13 @@
 
 package com.klikli_dev.theurgy.content.block.distiller;
 
-import com.klikli_dev.theurgy.content.block.UsableAlchemicalDeviceBlock;
-import com.klikli_dev.theurgy.content.block.calcinationoven.CalcinationOvenBlockEntity;
+import com.klikli_dev.theurgy.content.block.TwoSlotAlchemicalDeviceBlock;
 import com.klikli_dev.theurgy.registry.BlockEntityRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -31,7 +29,7 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.items.wrapper.RecipeWrapper;
 import org.jetbrains.annotations.Nullable;
 
-public class DistillerBlock extends Block implements EntityBlock, UsableAlchemicalDeviceBlock {
+public class DistillerBlock extends Block implements EntityBlock, TwoSlotAlchemicalDeviceBlock {
 
     public static final BooleanProperty LIT = BlockStateProperties.LIT;
 

57cb8220152af50a1a8bbcf95347840b5e294c17
Kli Kli <klikli@outlook.at>
2023-06-01 18:34:40 +0200

    feat: re-enable jei

--- src/main/java/com/klikli_dev/theurgy/integration/jei/DivinationRodSubtypeInterpreter.java
+++ src/main/java/com/klikli_dev/theurgy/integration/jei/DivinationRodSubtypeInterpreter.java
@@ -7,36 +7,35 @@
 package com.klikli_dev.theurgy.integration.jei;
 
 import com.klikli_dev.theurgy.TheurgyConstants;
-//import mezz.jei.api.ingredients.subtypes.IIngredientSubtypeInterpreter;
-//import mezz.jei.api.ingredients.subtypes.UidContext;
+import mezz.jei.api.ingredients.subtypes.IIngredientSubtypeInterpreter;
+import mezz.jei.api.ingredients.subtypes.UidContext;
 import net.minecraft.world.item.ItemStack;
 
-//TODO: reenable once jei updates
-//public class DivinationRodSubtypeInterpreter implements IIngredientSubtypeInterpreter<ItemStack> {
-//
-//    private static final DivinationRodSubtypeInterpreter instance = new DivinationRodSubtypeInterpreter();
-//
-//
-//    public static DivinationRodSubtypeInterpreter get() {
-//        return instance;
-//    }
-//
-//    @Override
-//    public String apply(ItemStack ingredient, UidContext context) {
-//        var tag = ingredient.getTag();
-//
-//        if(tag != null){
-//            var settingTier = tag.getInt(TheurgyConstants.Nbt.Divination.SETTING_TIER);
-//            var settingAllowedBlocksTag = tag.getString(TheurgyConstants.Nbt.Divination.SETTING_ALLOWED_BLOCKS_TAG);
-//            var settingDisallowedBlocksTag = tag.getString(TheurgyConstants.Nbt.Divination.SETTING_DISALLOWED_BLOCKS_TAG);
-//            var settingRange = tag.getInt(TheurgyConstants.Nbt.Divination.SETTING_RANGE);
-//            var settingDuration = tag.getInt(TheurgyConstants.Nbt.Divination.SETTING_DURATION);
-//            var settingDurability = tag.getInt(TheurgyConstants.Nbt.Divination.SETTING_DURABILITY);
-//            var settingAllowAttuning = tag.getBoolean(TheurgyConstants.Nbt.Divination.SETTING_ALLOW_ATTUNING);
-//
-//            return String.format("%s_%s_%s_%s_%s_%s_%s", settingTier, settingAllowedBlocksTag, settingDisallowedBlocksTag, settingRange, settingDuration, settingDurability, settingAllowAttuning);
-//        }
-//
-//        return IIngredientSubtypeInterpreter.NONE;
-//    }
-//}
+public class DivinationRodSubtypeInterpreter implements IIngredientSubtypeInterpreter<ItemStack> {
+
+    private static final DivinationRodSubtypeInterpreter instance = new DivinationRodSubtypeInterpreter();
+
+
+    public static DivinationRodSubtypeInterpreter get() {
+        return instance;
+    }
+
+    @Override
+    public String apply(ItemStack ingredient, UidContext context) {
+        var tag = ingredient.getTag();
+
+        if (tag != null) {
+            var settingTier = tag.getInt(TheurgyConstants.Nbt.Divination.SETTING_TIER);
+            var settingAllowedBlocksTag = tag.getString(TheurgyConstants.Nbt.Divination.SETTING_ALLOWED_BLOCKS_TAG);
+            var settingDisallowedBlocksTag = tag.getString(TheurgyConstants.Nbt.Divination.SETTING_DISALLOWED_BLOCKS_TAG);
+            var settingRange = tag.getInt(TheurgyConstants.Nbt.Divination.SETTING_RANGE);
+            var settingDuration = tag.getInt(TheurgyConstants.Nbt.Divination.SETTING_DURATION);
+            var settingDurability = tag.getInt(TheurgyConstants.Nbt.Divination.SETTING_DURABILITY);
+            var settingAllowAttuning = tag.getBoolean(TheurgyConstants.Nbt.Divination.SETTING_ALLOW_ATTUNING);
+
+            return String.format("%s_%s_%s_%s_%s_%s_%s", settingTier, settingAllowedBlocksTag, settingDisallowedBlocksTag, settingRange, settingDuration, settingDurability, settingAllowAttuning);
+        }
+
+        return IIngredientSubtypeInterpreter.NONE;
+    }
+}
--- src/main/java/com/klikli_dev/theurgy/integration/jei/JeiPlugin.java
+++ src/main/java/com/klikli_dev/theurgy/integration/jei/JeiPlugin.java
@@ -7,24 +7,21 @@
 package com.klikli_dev.theurgy.integration.jei;
 
 import com.klikli_dev.theurgy.Theurgy;
-import com.klikli_dev.theurgy.item.DivinationRodItem;
 import com.klikli_dev.theurgy.registry.ItemRegistry;
-//import mezz.jei.api.IModPlugin;
-//import mezz.jei.api.registration.ISubtypeRegistration;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.registration.ISubtypeRegistration;
 import net.minecraft.resources.ResourceLocation;
-import org.jetbrains.annotations.NotNull;
 
-//TODO: reenable once jei updates
-//@mezz.jei.api.JeiPlugin
-//public class JeiPlugin implements IModPlugin {
-//    @Override
-//    public ResourceLocation getPluginUid() {
-//        return Theurgy.loc("jei_plugin");
-//    }
-//
-//    @Override
-//    public void registerItemSubtypes(ISubtypeRegistration registration) {
-//        registration.registerSubtypeInterpreter(ItemRegistry.DIVINATION_ROD_T1.get(), DivinationRodSubtypeInterpreter.get());
-//        registration.registerSubtypeInterpreter(ItemRegistry.DIVINATION_ROD_T4.get(), DivinationRodSubtypeInterpreter.get());
-//    }
-//}
+@mezz.jei.api.JeiPlugin
+public class JeiPlugin implements IModPlugin {
+    @Override
+    public ResourceLocation getPluginUid() {
+        return Theurgy.loc("jei_plugin");
+    }
+
+    @Override
+    public void registerItemSubtypes(ISubtypeRegistration registration) {
+        registration.registerSubtypeInterpreter(ItemRegistry.DIVINATION_ROD_T1.get(), DivinationRodSubtypeInterpreter.get());
+        registration.registerSubtypeInterpreter(ItemRegistry.DIVINATION_ROD_T4.get(), DivinationRodSubtypeInterpreter.get());
+    }
+}

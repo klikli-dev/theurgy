a64222c148d37eb620fd37288fb952aaf9bb7490
Kli Kli <klikli@outlook.at>
2023-06-10 11:07:11 +0200

    fix: geckolib item render perspective as world entity

--- src/main/java/com/klikli_dev/theurgy/content/renderer/CalcinationOvenBEWLR.java
+++ src/main/java/com/klikli_dev/theurgy/content/renderer/CalcinationOvenBEWLR.java
@@ -14,6 +14,7 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.block.model.ItemTransform;
+import net.minecraft.world.item.ItemDisplayContext;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
@@ -44,14 +45,16 @@ public class CalcinationOvenBEWLR extends GeoItemRenderer<CalcinationOvenBlockIt
         this.itemRenderTranslations = new Matrix4f(poseStack.last().pose());
 
         if (this.scaleWidth != 1 && this.scaleHeight != 1) {
-            poseStack.scale(this.scaleWidth, this.scaleHeight, 1);
+            poseStack.scale(this.scaleWidth, this.scaleHeight, this.scaleWidth);
 
             //this is not as clean as I would like it - but it exactly centers the model for 0.5 scale :D
-            poseStack.translate(this.scaleWidth / 0.5 - 0.5, -0.1, 0);
+            poseStack.translate(this.scaleWidth / 0.5 - 0.5, -0.1, this.scaleWidth / 0.5 - 0.5);
         }
 
         poseStack.translate(0.5f, 0.51f, 0.5f);
 
-        this.transform.apply(false, poseStack);
+        if(this.renderPerspective == ItemDisplayContext.GUI) {
+            this.transform.apply(false, poseStack);
+        }
     }
 }
--- src/main/java/com/klikli_dev/theurgy/content/renderer/DistillerBEWLR.java
+++ src/main/java/com/klikli_dev/theurgy/content/renderer/DistillerBEWLR.java
@@ -12,6 +12,8 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.block.model.ItemTransform;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
@@ -35,6 +37,9 @@ public class DistillerBEWLR extends GeoItemRenderer<DistillerBlockItem> {
     @Override
     public void preRender(PoseStack poseStack, DistillerBlockItem animatable, BakedGeoModel model, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue,
                           float alpha) {
+
+        this.withScale(0.35f);
+
         //copied from parent, but fixed to also center the model when scaling
         this.itemRenderTranslations = new Matrix4f(poseStack.last().pose());
 
@@ -42,11 +47,13 @@ public class DistillerBEWLR extends GeoItemRenderer<DistillerBlockItem> {
             poseStack.scale(this.scaleWidth, this.scaleHeight, this.scaleWidth);
 
             //this is not as clean as I would like it - but it exactly centers the model for 0.35 scale :D
-            poseStack.translate(this.scaleWidth / 0.5 + 0.15, -0.1, this.scaleWidth / 0.5);
+            poseStack.translate(this.scaleWidth / 0.5 + 0.15, -0.1, this.scaleWidth / 0.5 + 0.15);
         }
 
         poseStack.translate(0.5f, 0.51f, 0.5f);
 
-        this.transform.apply(false, poseStack);
+        if(this.renderPerspective == ItemDisplayContext.GUI) {
+            this.transform.apply(false, poseStack);
+        }
     }
 }

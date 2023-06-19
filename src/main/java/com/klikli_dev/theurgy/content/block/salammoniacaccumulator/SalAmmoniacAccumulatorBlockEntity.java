75c5f73333b37f75963e1d034889f16b2098eb12
Kli Kli <klikli@outlook.at>
2023-06-19 10:43:40 +0200

    feat: make accumulator use crafting behaviour

--- src/main/java/com/klikli_dev/theurgy/content/block/salammoniacaccumulator/SalAmmoniacAccumulatorBlockEntity.java
+++ src/main/java/com/klikli_dev/theurgy/content/block/salammoniacaccumulator/SalAmmoniacAccumulatorBlockEntity.java
@@ -6,19 +6,16 @@
 
 package com.klikli_dev.theurgy.content.block.salammoniacaccumulator;
 
-import com.klikli_dev.theurgy.content.recipe.AccumulationRecipe;
-import com.klikli_dev.theurgy.content.recipe.wrapper.RecipeWrapperWithFluid;
 import com.klikli_dev.theurgy.registry.BlockEntityRegistry;
-import com.klikli_dev.theurgy.registry.RecipeTypeRegistry;
-import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.Connection;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.RecipeManager;
-import net.minecraft.world.item.crafting.RecipeType;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
@@ -29,16 +26,15 @@ import net.minecraftforge.fluids.FluidType;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.fluids.capability.templates.FluidTank;
 import net.minecraftforge.items.IItemHandler;
-import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Optional;
+import java.util.function.Predicate;
 
 public class SalAmmoniacAccumulatorBlockEntity extends BlockEntity {
 
-    private final CachedCheck recipeCachedCheck;
+    private final SalAmmoniacAccumulatorCraftingBehaviour craftingBehaviour;
 
     public ItemStackHandler inventory;
     public LazyOptional<IItemHandler> inventoryCapability;
@@ -46,12 +42,8 @@ public class SalAmmoniacAccumulatorBlockEntity extends BlockEntity {
     public FluidTank waterTank;
     public LazyOptional<IFluidHandler> waterTankCapability;
 
-    public RecipeWrapperWithFluid recipeWrapper;
-
     private boolean checkOutputTankOnNextQuery;
     private boolean hasOutputTank;
-    private int progress;
-    private int totalTime;
 
     public SalAmmoniacAccumulatorBlockEntity(BlockPos pPos, BlockState pState) {
         super(BlockEntityRegistry.SAL_AMMONIAC_ACCUMULATOR.get(), pPos, pState);
@@ -59,20 +51,57 @@ public class SalAmmoniacAccumulatorBlockEntity extends BlockEntity {
         this.inventory = new Inventory();
         this.inventoryCapability = LazyOptional.of(() -> this.inventory);
 
-        //fluid tank has a validator that can use our canProcess, our inventory instead has to override isItemValid
-        this.waterTank = new FluidTank(FluidType.BUCKET_VOLUME, (this::canProcess)) {
-            @Override
-            protected void onContentsChanged() {
-                SalAmmoniacAccumulatorBlockEntity.this.setChanged();
-            }
-        };
+        this.craftingBehaviour = new SalAmmoniacAccumulatorCraftingBehaviour(this, () -> this.inventory, () -> this.inventory, () -> this.waterTank, () -> this.getOutputTank());
+
+        this.waterTank = new WaterTank(FluidType.BUCKET_VOLUME, this.craftingBehaviour::canProcess);
         this.waterTankCapability = LazyOptional.of(() -> this.waterTank);
 
-        this.recipeCachedCheck = new CachedCheck(RecipeTypeRegistry.ACCUMULATION.get());
-
-        this.recipeWrapper = new RecipeWrapperWithFluid(this.inventory, this.waterTank);
-
         this.checkOutputTankOnNextQuery = true;
+    }
+
+    @Override
+    public CompoundTag getUpdateTag() {
+        var tag = new CompoundTag();
+        this.writeNetwork(tag);
+        return tag;
+    }
+
+    @Override
+    public void handleUpdateTag(CompoundTag tag) {
+        this.readNetwork(tag);
+    }
+
+    @Nullable
+    @Override
+    public Packet<ClientGamePacketListener> getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    @Override
+    public void onDataPacket(Connection connection, ClientboundBlockEntityDataPacket packet) {
+        var tag = packet.getTag();
+        if (tag != null) {
+            this.readNetwork(tag);
+        }
+    }
+
+    public void readNetwork(CompoundTag tag) {
+        if (tag.contains("waterTank")) {
+            this.waterTank.readFromNBT(tag.getCompound("waterTank"));
+        }
+
+        this.craftingBehaviour.readNetwork(tag);
+    }
+
+    public void writeNetwork(CompoundTag tag) {
+        tag.put("waterTank", this.waterTank.writeToNBT(new CompoundTag()));
+
+        this.craftingBehaviour.writeNetwork(tag);
+    }
+
+    public void sendBlockUpdated() {
+        if (this.level != null && !this.level.isClientSide)
+            this.level.sendBlockUpdated(this.getBlockPos(), this.getBlockState(), this.getBlockState(), Block.UPDATE_CLIENTS);
     }
 
     public void tickServer() {
@@ -82,64 +111,7 @@ public class SalAmmoniacAccumulatorBlockEntity extends BlockEntity {
 
         boolean hasInput = !this.waterTank.isEmpty();
 
-        if (hasInput) {
-            //only even check for recipe if we have input to avoid unnecessary lookups
-            var recipe = this.recipeCachedCheck.getRecipeFor(this.recipeWrapper, this.level).orElse(null);
-
-            //if we are lit and have a recipe, update progress
-            if (this.canCraft(recipe)) {
-                ++this.progress;
-
-                //if we hit max progress, craft the item and reset progress
-                if (this.progress >= this.totalTime) {
-                    this.progress = 0;
-                    this.totalTime = this.getTotalTime();
-                    this.craft(recipe);
-                    //TODO: advancement?
-                }
-            } else {
-                this.progress = 0;
-            }
-        }
-    }
-
-
-    private boolean canCraft(@Nullable AccumulationRecipe pRecipe) {
-        if (pRecipe == null) return false;
-
-        var assembledStack = pRecipe.assembleFluid(this.recipeWrapper, this.getLevel().registryAccess());
-        if (assembledStack.isEmpty()) {
-            return false;
-        } else {
-            var tank = this.getOutputTank();
-            int fluidAccepted = tank.fill(assembledStack, IFluidHandler.FluidAction.SIMULATE);
-
-            return fluidAccepted == assembledStack.getAmount(); // only allow if we have space for the entire fluid
-        }
-
-    }
-
-    private boolean craft(@Nullable AccumulationRecipe pRecipe) {
-        if (!this.canCraft(pRecipe)) return false;
-
-        FluidStack assembledFluid = pRecipe.assembleFluid(this.recipeWrapper, this.getLevel().registryAccess());
-        IFluidHandler outputFluidTank = this.getOutputTank();
-
-        outputFluidTank.fill(assembledFluid, IFluidHandler.FluidAction.EXECUTE);
-
-        //only consume the solid solute, if the recipe requires it.
-        //this avoids accidentally consuming a solute when a "water only" recipe is running while the solute is added.
-        if (pRecipe.hasSolute()) {
-            this.inventory.extractItem(0, 1, false);
-        }
-
-        this.waterTank.drain(pRecipe.getEvaporant().getAmount(), IFluidHandler.FluidAction.EXECUTE);
-
-        return true;
-    }
-
-    protected int getTotalTime() {
-        return this.recipeCachedCheck.getRecipeFor(this.recipeWrapper, this.level).map(AccumulationRecipe::getAccumulationTime).orElse(AccumulationRecipe.DEFAULT_ACCUMULATION_TIME);
+        this.craftingBehaviour.tickServer(true, hasInput); //does not need heat
     }
 
     public IFluidHandler getOutputTank() {
@@ -178,10 +150,9 @@ public class SalAmmoniacAccumulatorBlockEntity extends BlockEntity {
         super.saveAdditional(pTag);
 
         pTag.put("inventory", this.inventory.serializeNBT());
-        pTag.putShort("progress", (short) this.progress);
-        var waterTankTag = new CompoundTag();
-        this.waterTank.writeToNBT(waterTankTag);
-        pTag.put("waterTank", waterTankTag);
+        pTag.put("waterTank", this.waterTank.writeToNBT(new CompoundTag()));
+
+        this.craftingBehaviour.saveAdditional(pTag);
     }
 
     @Override
@@ -189,114 +160,22 @@ public class SalAmmoniacAccumulatorBlockEntity extends BlockEntity {
         super.load(pTag);
 
         if (pTag.contains("inventory")) this.inventory.deserializeNBT(pTag.getCompound("inventory"));
-        if (pTag.contains("progress")) this.progress = pTag.getShort("progress");
 
         if (pTag.contains("waterTank")) {
-            var waterTankTag = pTag.getCompound("waterTank");
-            this.waterTank.readFromNBT(waterTankTag);
+            this.waterTank.readFromNBT(pTag.getCompound("waterTank"));
         }
     }
 
-    private boolean canProcess(ItemStack stack) {
-        if (ItemHandlerHelper.canItemStacksStack(stack, this.inventory.getStackInSlot(0)))
-            return true; //early out if we are already processing this type of item
+    public class WaterTank extends FluidTank {
 
-        //now we use our custom cached check that ignores liquids:
-        return this.recipeCachedCheck.getRecipeFor(stack, this.level).isPresent();
-    }
-
-    private boolean canProcess(FluidStack stack) {
-        if (this.waterTank.getFluid().isFluidEqual(stack))
-            return true; //early out if we are already processing this type of fluid
-
-        //now we use our custom cached check that checks only liquids:
-        return this.recipeCachedCheck.getRecipeFor(stack, this.level).isPresent();
-    }
-
-    /**
-     * A custom cached check
-     */
-    private static class CachedCheck implements RecipeManager.CachedCheck<RecipeWrapperWithFluid, AccumulationRecipe> {
-
-        private final RecipeType<AccumulationRecipe> type;
-        private final RecipeManager.CachedCheck<RecipeWrapperWithFluid, AccumulationRecipe> internal;
-        @Nullable
-        private ResourceLocation lastRecipe;
-
-        public CachedCheck(RecipeType<AccumulationRecipe> type) {
-            this.type = type;
-            this.internal = RecipeManager.createCheck(type);
+        public WaterTank(int capacity, Predicate<FluidStack> validator) {
+            super(capacity, validator);
         }
 
-        private Optional<Pair<ResourceLocation, AccumulationRecipe>> getRecipeFor(ItemStack stack, Level level, @Nullable ResourceLocation lastRecipe) {
-
-            var recipeManager = level.getRecipeManager();
-            var map = recipeManager.byType(this.type);
-            if (lastRecipe != null) {
-                var recipe = map.get(lastRecipe);
-                //test only the ingredient without the (separate) evaporate fluid ingredient check that the recipe.matches() would.
-                if (recipe != null && recipe.hasSolute() && recipe.getSolute().test(stack)) {
-                    return Optional.of(Pair.of(lastRecipe, recipe));
-                }
-            }
-
-            return map.entrySet().stream().filter((entry) -> entry.getValue().hasSolute() && entry.getValue().getSolute().test(stack)).findFirst().map((entry) -> Pair.of(entry.getKey(), entry.getValue()));
-        }
-
-        private Optional<Pair<ResourceLocation, AccumulationRecipe>> getRecipeFor(FluidStack stack, Level level, @Nullable ResourceLocation lastRecipe) {
-
-            var recipeManager = level.getRecipeManager();
-            var map = recipeManager.byType(this.type);
-            if (lastRecipe != null) {
-                var recipe = map.get(lastRecipe);
-                //test only the fluid without the (separate) solute item ingredient check that the recipe.matches() would.
-                if (recipe != null && recipe.getEvaporant().test(stack)) {
-                    return Optional.of(Pair.of(lastRecipe, recipe));
-                }
-            }
-
-            return map.entrySet().stream().filter((entry) -> entry.getValue().getEvaporant().test(stack)).findFirst().map((entry) -> Pair.of(entry.getKey(), entry.getValue()));
-        }
-
-        /**
-         * This only checks ingredients, not fluids
-         */
-        public Optional<AccumulationRecipe> getRecipeFor(ItemStack stack, Level level) {
-            var optional = this.getRecipeFor(stack, level, this.lastRecipe);
-            if (optional.isPresent()) {
-                var pair = optional.get();
-                this.lastRecipe = pair.getFirst();
-                return Optional.of(pair.getSecond());
-            } else {
-                return Optional.empty();
-            }
-        }
-
-        /**
-         * This only checks fluids, not ingredients
-         */
-        public Optional<AccumulationRecipe> getRecipeFor(FluidStack stack, Level level) {
-            var optional = this.getRecipeFor(stack, level, this.lastRecipe);
-            if (optional.isPresent()) {
-                var pair = optional.get();
-                this.lastRecipe = pair.getFirst();
-                return Optional.of(pair.getSecond());
-            } else {
-                return Optional.empty();
-            }
-        }
-
-        /**
-         * This checks full recipe validity: ingredients + fluids
-         */
         @Override
-        public Optional<AccumulationRecipe> getRecipeFor(RecipeWrapperWithFluid container, Level level) {
-            var recipe = this.internal.getRecipeFor(container, level);
-            if (recipe.isPresent()) {
-                this.lastRecipe = recipe.get().getId();
-            }
-
-            return recipe;
+        protected void onContentsChanged() {
+            SalAmmoniacAccumulatorBlockEntity.this.setChanged();
+            SalAmmoniacAccumulatorBlockEntity.this.sendBlockUpdated();
         }
     }
 
@@ -317,16 +196,13 @@ public class SalAmmoniacAccumulatorBlockEntity extends BlockEntity {
             super.setStackInSlot(slot, newStack);
 
             if (!sameItem) {
-                SalAmmoniacAccumulatorBlockEntity.this.totalTime = SalAmmoniacAccumulatorBlockEntity.this.getTotalTime();
-                SalAmmoniacAccumulatorBlockEntity.this.progress = 0;
-                SalAmmoniacAccumulatorBlockEntity.this.setChanged();
+                SalAmmoniacAccumulatorBlockEntity.this.craftingBehaviour.onInputItemChanged(oldStack, newStack);
             }
-
         }
 
         @Override
         public boolean isItemValid(int slot, ItemStack stack) {
-            return SalAmmoniacAccumulatorBlockEntity.this.canProcess(stack) && super.isItemValid(slot, stack);
+            return SalAmmoniacAccumulatorBlockEntity.this.craftingBehaviour.canProcess(stack) && super.isItemValid(slot, stack);
         }
 
         @Override
---
+++ src/main/java/com/klikli_dev/theurgy/content/block/salammoniacaccumulator/SalAmmoniacAccumulatorCraftingBehaviour.java
@@ -0,0 +1,108 @@
+/*
+ * SPDX-FileCopyrightText: 2023 klikli-dev
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.klikli_dev.theurgy.content.block.salammoniacaccumulator;
+
+import com.klikli_dev.theurgy.content.block.behaviour.CraftingBehaviour;
+import com.klikli_dev.theurgy.content.recipe.AccumulationRecipe;
+import com.klikli_dev.theurgy.content.recipe.wrapper.RecipeWrapperWithFluid;
+import com.klikli_dev.theurgy.registry.RecipeTypeRegistry;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.common.util.Lazy;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.IFluidTank;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.ItemHandlerHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Supplier;
+
+public class SalAmmoniacAccumulatorCraftingBehaviour extends CraftingBehaviour<RecipeWrapperWithFluid, AccumulationRecipe, SalAmmoniacAccumulatorCachedCheck> {
+
+    protected Supplier<IFluidHandler> waterTankSupplier;
+    protected Supplier<IFluidHandler> outputTankSupplier;
+
+    public SalAmmoniacAccumulatorCraftingBehaviour(BlockEntity blockEntity, Supplier<IItemHandlerModifiable> inputInventorySupplier, Supplier<IItemHandlerModifiable> outputInventorySupplier, Supplier<IFluidHandler> waterTankSupplier, Supplier<IFluidHandler> outputTankSupplier) {
+        super(blockEntity,
+                Lazy.of(() -> new RecipeWrapperWithFluid(inputInventorySupplier.get(), waterTankSupplier.get())),
+                inputInventorySupplier,
+                outputInventorySupplier,
+                new SalAmmoniacAccumulatorCachedCheck(RecipeTypeRegistry.ACCUMULATION.get()));
+
+        this.waterTankSupplier = waterTankSupplier;
+        this.outputTankSupplier = outputTankSupplier;
+    }
+
+    @Override
+    public boolean canProcess(ItemStack stack) {
+        if (ItemHandlerHelper.canItemStacksStack(stack, this.inputInventorySupplier.get().getStackInSlot(0)))
+            return true; //early out if we are already processing this type of item
+
+
+        return this.recipeCachedCheck.getRecipeFor(stack, this.blockEntity.getLevel()).isPresent();
+    }
+
+
+    public boolean canProcess(FluidStack stack) {
+        if (this.waterTankSupplier.get().getFluidInTank(0).isFluidEqual(stack))
+            return true; //early out if we are already processing this type of fluid
+
+        //now we use our custom cached check that checks only liquids:
+        return this.recipeCachedCheck.getRecipeFor(stack, this.blockEntity.getLevel()).isPresent();
+    }
+
+    @Override
+    protected int getIngredientCount(AccumulationRecipe recipe) {
+        return 1;
+    }
+
+    @Override
+    protected int getCraftingTime(AccumulationRecipe recipe) {
+        return recipe.getAccumulationTime();
+    }
+
+    @Override
+    protected int getDefaultCraftingTime() {
+        return AccumulationRecipe.DEFAULT_ACCUMULATION_TIME;
+    }
+
+    @Override
+    protected boolean canCraft(@Nullable AccumulationRecipe pRecipe) {
+        if (pRecipe == null) return false;
+
+        var assembledStack = pRecipe.assembleFluid(this.recipeWrapperSupplier.get(), this.blockEntity.getLevel().registryAccess());
+        if (assembledStack.isEmpty()) {
+            return false;
+        } else {
+            var tank = this.outputTankSupplier.get();
+            int fluidAccepted = tank.fill(assembledStack, IFluidHandler.FluidAction.SIMULATE);
+
+            return fluidAccepted == assembledStack.getAmount(); // only allow if we have space for the entire fluid
+        }
+    }
+
+    @Override
+    protected boolean craft(@Nullable AccumulationRecipe pRecipe) {
+        if (!this.canCraft(pRecipe)) return false;
+
+        var assembledFluid = pRecipe.assembleFluid(this.recipeWrapperSupplier.get(), this.blockEntity.getLevel().registryAccess());
+        var outputFluidTank = this.outputTankSupplier.get();
+
+        outputFluidTank.fill(assembledFluid, IFluidHandler.FluidAction.EXECUTE);
+
+        //only consume the solid solute, if the recipe requires it.
+        //this avoids accidentally consuming a solute when a "water only" recipe is running while the solute is added.
+        if (pRecipe.hasSolute()) {
+            this.inputInventorySupplier.get().extractItem(0, this.getIngredientCount(pRecipe), false);
+        }
+
+        this.waterTankSupplier.get().drain(pRecipe.getEvaporant().getAmount(), IFluidHandler.FluidAction.EXECUTE);
+
+        return true;
+    }
+}

067f2cd9d7351ad956380cf645add703051f6d11
Kli Kli <klikli@outlook.at>
2023-12-16 15:49:37 +0100

    feat: make changes in source/target pedestal notify crafting behaviour

--- src/main/java/com/klikli_dev/theurgy/content/apparatus/reformationarray/ReformationSourcePedestalBlockEntity.java
+++ src/main/java/com/klikli_dev/theurgy/content/apparatus/reformationarray/ReformationSourcePedestalBlockEntity.java
@@ -81,6 +81,38 @@ public class ReformationSourcePedestalBlockEntity extends BlockEntity {
         }
 
         @Override
+        public void setStackInSlot(int slot, @NotNull ItemStack newStack) {
+            var oldStack = this.getStackInSlot(slot);
+
+            boolean sameItem = !newStack.isEmpty() && ItemStack.isSameItemSameTags(newStack, oldStack);
+
+            super.setStackInSlot(slot, newStack);
+
+            if (!sameItem) {
+                var emitter = ReformationSourcePedestalBlockEntity.this.sulfuricFluxEmitter;
+                if (emitter != null && emitter.get() != null)
+                    emitter.get().craftingBehaviour.onInputItemChanged(oldStack, newStack);
+            }
+        }
+
+        @Override
+        public @NotNull ItemStack insertItem(int slot, @NotNull ItemStack newStack, boolean simulate) {
+            if (!simulate) {
+                var oldStack = this.getStackInSlot(slot);
+                var result = super.insertItem(slot, newStack, simulate);
+
+                if (result != newStack) {
+                    var emitter = ReformationSourcePedestalBlockEntity.this.sulfuricFluxEmitter;
+                    if (emitter != null && emitter.get() != null)
+                        emitter.get().craftingBehaviour.onInputItemChanged(oldStack, newStack);
+                }
+
+                return result;
+            }
+            return super.insertItem(slot, newStack, simulate);
+        }
+
+        @Override
         public boolean isItemValid(int slot, ItemStack stack) {
             return stack.is(ItemTagRegistry.ALCHEMICAL_SULFURS) && super.isItemValid(slot, stack);
         }
@@ -90,7 +122,7 @@ public class ReformationSourcePedestalBlockEntity extends BlockEntity {
             ReformationSourcePedestalBlockEntity.this.setChanged();
             var emitter = ReformationSourcePedestalBlockEntity.this.sulfuricFluxEmitter;
 
-            if(emitter != null && emitter.get() != null)
+            if (emitter != null && emitter.get() != null)
                 emitter.get().onSourcePedestalContentChange(ReformationSourcePedestalBlockEntity.this);
         }
     }
--- src/main/java/com/klikli_dev/theurgy/content/apparatus/reformationarray/ReformationTargetPedestalBlockEntity.java
+++ src/main/java/com/klikli_dev/theurgy/content/apparatus/reformationarray/ReformationTargetPedestalBlockEntity.java
@@ -81,6 +81,38 @@ public class ReformationTargetPedestalBlockEntity extends BlockEntity {
         }
 
         @Override
+        public void setStackInSlot(int slot, @NotNull ItemStack newStack) {
+            var oldStack = this.getStackInSlot(slot);
+
+            boolean sameItem = !newStack.isEmpty() && ItemStack.isSameItemSameTags(newStack, oldStack);
+
+            super.setStackInSlot(slot, newStack);
+
+            if (!sameItem) {
+                var emitter = ReformationTargetPedestalBlockEntity.this.sulfuricFluxEmitter;
+                if (emitter != null && emitter.get() != null)
+                    emitter.get().craftingBehaviour.onInputItemChanged(oldStack, newStack);
+            }
+        }
+
+        @Override
+        public @NotNull ItemStack insertItem(int slot, @NotNull ItemStack newStack, boolean simulate) {
+            if (!simulate) {
+                var oldStack = this.getStackInSlot(slot);
+                var result = super.insertItem(slot, newStack, simulate);
+
+                if (result != newStack) {
+                    var emitter = ReformationTargetPedestalBlockEntity.this.sulfuricFluxEmitter;
+                    if (emitter != null && emitter.get() != null)
+                        emitter.get().craftingBehaviour.onInputItemChanged(oldStack, newStack);
+                }
+
+                return result;
+            }
+            return super.insertItem(slot, newStack, simulate);
+        }
+
+        @Override
         public boolean isItemValid(int slot, ItemStack stack) {
             return stack.is(ItemTagRegistry.ALCHEMICAL_SULFURS) && super.isItemValid(slot, stack);
         }
@@ -90,7 +122,7 @@ public class ReformationTargetPedestalBlockEntity extends BlockEntity {
             ReformationTargetPedestalBlockEntity.this.setChanged();
             var emitter = ReformationTargetPedestalBlockEntity.this.sulfuricFluxEmitter;
 
-            if(emitter != null && emitter.get() != null)
+            if (emitter != null && emitter.get() != null)
                 emitter.get().onTargetPedestalContentChange(ReformationTargetPedestalBlockEntity.this);
         }
     }

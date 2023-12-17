1f5184c11a823a93b1b2f2d273386b41468a1723
Kli Kli <klikli@outlook.at>
2023-12-17 16:08:07 +0100

    feat: refactor inventory notify stuff

--- src/main/java/com/klikli_dev/theurgy/content/apparatus/reformationarray/ReformationSourcePedestalBlockEntity.java
+++ src/main/java/com/klikli_dev/theurgy/content/apparatus/reformationarray/ReformationSourcePedestalBlockEntity.java
@@ -1,21 +1,19 @@
 package com.klikli_dev.theurgy.content.apparatus.reformationarray;
 
+import com.klikli_dev.theurgy.content.behaviour.MonitoredItemStackHandler;
 import com.klikli_dev.theurgy.content.particle.ParticleColor;
 import com.klikli_dev.theurgy.content.particle.glow.GlowParticleProvider;
 import com.klikli_dev.theurgy.registry.BlockEntityRegistry;
 import com.klikli_dev.theurgy.registry.ItemTagRegistry;
-import io.netty.handler.codec.EncoderException;
-import net.minecraft.Util;
+import com.klikli_dev.theurgy.util.BlockEntityUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.NbtOps;
 import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
@@ -48,14 +46,14 @@ public class ReformationSourcePedestalBlockEntity extends BlockEntity {
         this.sulfuricFluxEmitter = new WeakReference<>(sulfuricFluxEmitter);
     }
 
-    public void tickClient(){
-        if(this.showParticles && this.level.getRandom().nextFloat() < 0.07f){
+    public void tickClient() {
+        if (this.showParticles && this.level.getRandom().nextFloat() < 0.07f) {
             var pos = this.getBlockPos();
             this.level.addParticle(GlowParticleProvider.createOptions(
                     ParticleColor.fromInt(0xFF00FF),
                     0.5f,
                     0.75f,
-                    200), pos.getX() + 0.5f, pos.getY() + 1.0f, pos.getZ()+ 0.5f, 0, 0, 0);
+                    200), pos.getX() + 0.5f, pos.getY() + 1.0f, pos.getZ() + 0.5f, 0, 0, 0);
         }
     }
 
@@ -134,71 +132,46 @@ public class ReformationSourcePedestalBlockEntity extends BlockEntity {
         pTag.putBoolean("showParticles", this.showParticles);
     }
 
-
-    public class InputInventory extends ItemStackHandler {
+    /**
+     * Notification behaviour:
+     * if the inventory becomes empty or non-empty or item type changes, do a network update
+     * otherwise just setChanged() -> handled via onContentsChanged
+     */
+    public class InputInventory extends MonitoredItemStackHandler {
 
         public InputInventory() {
             super(1);
         }
 
+        protected SulfuricFluxEmitterBlockEntity emitter() {
+            return ReformationSourcePedestalBlockEntity.this.sulfuricFluxEmitter != null ? ReformationSourcePedestalBlockEntity.this.sulfuricFluxEmitter.get() : null;
+        }
+
         @Override
-        public void setStackInSlot(int slot, @NotNull ItemStack newStack) {
-            var oldStack = this.getStackInSlot(slot);
-
-            boolean sameItem = !newStack.isEmpty() && ItemStack.isSameItemSameTags(newStack, oldStack);
-
-            super.setStackInSlot(slot, newStack);
-
-            if (!sameItem) {
-                ReformationSourcePedestalBlockEntity.this.level.sendBlockUpdated(
-                        ReformationSourcePedestalBlockEntity.this.getBlockPos(),
-                        ReformationSourcePedestalBlockEntity.this.getBlockState(),
-                        ReformationSourcePedestalBlockEntity.this.getBlockState(),
-                        Block.UPDATE_CLIENTS);
-                var emitter = ReformationSourcePedestalBlockEntity.this.sulfuricFluxEmitter;
-                if (emitter != null && emitter.get() != null)
-                    emitter.get().craftingBehaviour.onInputItemChanged(oldStack, newStack);
+        protected void onSetStackInSlot(int slot, ItemStack oldStack, ItemStack newStack, boolean isSameItem) {
+            if (!isSameItem) {
+                BlockEntityUtil.sendBlockUpdated(ReformationSourcePedestalBlockEntity.this);
+                if (this.emitter() != null)
+                    this.emitter().craftingBehaviour.onInputItemChanged(oldStack, newStack);
             }
         }
 
         @Override
-        public @NotNull ItemStack insertItem(int slot, @NotNull ItemStack newStack, boolean simulate) {
-            if (!simulate) {
-                var oldStack = this.getStackInSlot(slot);
-                var result = super.insertItem(slot, newStack, simulate);
-
-                if (result != newStack) {
-                    ReformationSourcePedestalBlockEntity.this.level.sendBlockUpdated(
-                            ReformationSourcePedestalBlockEntity.this.getBlockPos(),
-                            ReformationSourcePedestalBlockEntity.this.getBlockState(),
-                            ReformationSourcePedestalBlockEntity.this.getBlockState(),
-                            Block.UPDATE_CLIENTS);
-                    var emitter = ReformationSourcePedestalBlockEntity.this.sulfuricFluxEmitter;
-                    if (emitter != null && emitter.get() != null)
-                        emitter.get().craftingBehaviour.onInputItemChanged(oldStack, newStack);
-                }
-
-                return result;
+        protected void onInsertItem(int slot, ItemStack oldStack, ItemStack newStack, ItemStack toInsert, ItemStack remaining) {
+            if (oldStack.isEmpty() && !newStack.isEmpty()) {
+                BlockEntityUtil.sendBlockUpdated(ReformationSourcePedestalBlockEntity.this);
+                if (this.emitter() != null)
+                    this.emitter().craftingBehaviour.onInputItemChanged(oldStack, newStack);
             }
-            return super.insertItem(slot, newStack, simulate);
         }
 
         @Override
-        public @NotNull ItemStack extractItem(int slot, int amount, boolean simulate) {
-            if (!simulate) {
-                var result = super.extractItem(slot, amount, simulate);
-
-                if (this.getStackInSlot(slot).isEmpty()) {
-                    ReformationSourcePedestalBlockEntity.this.level.sendBlockUpdated(
-                            ReformationSourcePedestalBlockEntity.this.getBlockPos(),
-                            ReformationSourcePedestalBlockEntity.this.getBlockState(),
-                            ReformationSourcePedestalBlockEntity.this.getBlockState(),
-                            Block.UPDATE_CLIENTS);
-                }
-
-                return result;
+        protected void onExtractItem(int slot, ItemStack oldStack, ItemStack newStack, ItemStack extracted) {
+            if (newStack.isEmpty()) {
+                BlockEntityUtil.sendBlockUpdated(ReformationSourcePedestalBlockEntity.this);
+                if (this.emitter() != null)
+                    this.emitter().craftingBehaviour.onInputItemChanged(oldStack, newStack);
             }
-            return super.extractItem(slot, amount, simulate);
         }
 
         @Override
@@ -209,10 +182,9 @@ public class ReformationSourcePedestalBlockEntity extends BlockEntity {
         @Override
         protected void onContentsChanged(int slot) {
             ReformationSourcePedestalBlockEntity.this.setChanged();
-            var emitter = ReformationSourcePedestalBlockEntity.this.sulfuricFluxEmitter;
 
-            if (emitter != null && emitter.get() != null)
-                emitter.get().onSourcePedestalContentChange(ReformationSourcePedestalBlockEntity.this);
+            if (this.emitter() != null)
+                this.emitter().onSourcePedestalContentChange(ReformationSourcePedestalBlockEntity.this);
         }
     }
 }

ea9efbdd7bcd8a55472f84a87c6f7ed06446d1e1
Kli Kli <klikli@outlook.at>
2023-12-17 11:56:49 +0100

    feat: add content dropping

--- src/main/java/com/klikli_dev/theurgy/content/apparatus/reformationarray/ReformationSourcePedestalBlock.java
+++ src/main/java/com/klikli_dev/theurgy/content/apparatus/reformationarray/ReformationSourcePedestalBlock.java
@@ -1,9 +1,11 @@
 package com.klikli_dev.theurgy.content.apparatus.reformationarray;
 
+import com.klikli_dev.theurgy.content.apparatus.pyromanticbrazier.PyromanticBrazierBlockEntity;
 import com.klikli_dev.theurgy.content.behaviour.ItemHandlerBehaviour;
 import com.klikli_dev.theurgy.content.behaviour.OneSlotItemHandlerBehaviour;
 import com.klikli_dev.theurgy.registry.BlockEntityRegistry;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -13,6 +15,7 @@ import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.items.wrapper.RecipeWrapper;
 import org.jetbrains.annotations.Nullable;
 
 public class ReformationSourcePedestalBlock extends Block implements EntityBlock {
@@ -36,6 +39,17 @@ public class ReformationSourcePedestalBlock extends Block implements EntityBlock
         }
 
         return InteractionResult.PASS;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
+        if (!pState.is(pNewState.getBlock())) {
+            if (pLevel.getBlockEntity(pPos) instanceof ReformationSourcePedestalBlockEntity blockEntity) {
+                Containers.dropContents(pLevel, pPos, new RecipeWrapper(blockEntity.inputInventory));
+            }
+        }
+        super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
     }
 
     @Nullable

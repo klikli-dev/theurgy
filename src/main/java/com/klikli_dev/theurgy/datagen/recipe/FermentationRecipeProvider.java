b29e619d078860db52905c6ec611a0ba1926b3d2
Kli Kli <klikli@outlook.at>
2024-01-05 15:28:43 +0100

    feat: add recipe providers

---
+++ src/main/java/com/klikli_dev/theurgy/datagen/recipe/FermentationRecipeProvider.java
@@ -0,0 +1,73 @@
+// SPDX-FileCopyrightText: 2022 klikli-dev
+//
+// SPDX-License-Identifier: MIT
+
+package com.klikli_dev.theurgy.datagen.recipe;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.klikli_dev.theurgy.Theurgy;
+import com.klikli_dev.theurgy.content.recipe.FermentationRecipe;
+import com.klikli_dev.theurgy.registry.RecipeTypeRegistry;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.material.Fluid;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+
+public class FermentationRecipeProvider extends JsonRecipeProvider {
+
+    public static final int TIME = FermentationRecipe.TIME;
+
+    public FermentationRecipeProvider(PackOutput packOutput) {
+        super(packOutput, Theurgy.MODID, "fermentation");
+    }
+
+    @Override
+    void buildRecipes(BiConsumer<ResourceLocation, JsonObject> recipeConsumer) {
+
+    }
+
+    public void makeRecipe(String name, Fluid fluid, int fluidAmount, List<TagKey<Item>> ingredients, Item result, int resultCount, int time) {
+        var recipe = this.makeRecipeJson(
+                this.makeFluidTagIngredient(this.locFor(fluid)),
+                fluidAmount,
+                ingredients.stream().map(i -> this.makeTagIngredient(this.locFor(i))).toList(),
+                this.makeItemStackCodecResult(this.locFor(result), resultCount),
+                time);
+
+        var conditions = new JsonArray();
+        for (var ingredient : ingredients) {
+            conditions.add(this.makeTagNotEmptyCondition(ingredient.location().toString()));
+        }
+        recipe.add("conditions", conditions);
+
+        this.recipeConsumer.accept(this.modLoc(name), recipe);
+    }
+
+
+    public JsonObject makeRecipeJson(JsonObject fluid, int fluidAmount, List<JsonObject> ingredients, JsonObject result, int time) {
+        var ingredientsArray = new JsonArray();
+        for (var ingredient : ingredients) {
+            ingredientsArray.add(ingredient);
+        }
+
+        var recipe = new JsonObject();
+        recipe.addProperty("type", RecipeTypeRegistry.FERMENTATION.getId().toString());
+        recipe.add("fluid", fluid);
+        recipe.addProperty("fluidAmount", fluidAmount);
+        recipe.add("ingredients", ingredientsArray);
+        recipe.add("result", result);
+        recipe.addProperty("time", time);
+        return recipe;
+    }
+
+
+    @Override
+    public String getName() {
+        return "Fermentation Recipes";
+    }
+}

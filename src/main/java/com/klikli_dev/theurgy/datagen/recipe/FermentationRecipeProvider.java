b8f7db43979d0fc9d1a8384de702f3b073a3cd20
Kli Kli <klikli@outlook.at>
2024-01-07 15:38:34 +0100

    feat: add crop.based fermentation recipes

--- src/main/java/com/klikli_dev/theurgy/datagen/recipe/FermentationRecipeProvider.java
+++ src/main/java/com/klikli_dev/theurgy/datagen/recipe/FermentationRecipeProvider.java
@@ -17,6 +17,7 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraftforge.common.Tags;
 
 import java.util.List;
 import java.util.function.BiConsumer;
@@ -31,29 +32,79 @@ public class FermentationRecipeProvider extends JsonRecipeProvider {
 
     @Override
     void buildRecipes(BiConsumer<ResourceLocation, JsonObject> recipeConsumer) {
+        this.makeRecipesForCropTag(ItemTagRegistry.SUGAR);
+        this.makeRecipesForCropTag(Tags.Items.CROPS);
+    }
+    
+    public void makeRecipesForCropTag(TagKey<Item> cropTag) {
         this.makeRecipe(Fluids.WATER, 125, List.of(
                 ItemTagRegistry.ALCHEMICAL_SULFURS_GEMS_ABUNDANT,
-                ItemTagRegistry.SUGAR
-        ), SulfurRegistry.GEMS_ABUNDANT.get(), 1, TIME);
+                cropTag
+        ), SulfurRegistry.GEMS_ABUNDANT.get(), 1, TIME, "_using_" + this.name(cropTag));
 
         this.makeRecipe(Fluids.WATER, 250, List.of(
                 ItemTagRegistry.ALCHEMICAL_SULFURS_GEMS_COMMON,
-                ItemTagRegistry.SUGAR
-        ), SulfurRegistry.GEMS_COMMON.get(), 1, TIME);
+                cropTag
+        ), SulfurRegistry.GEMS_COMMON.get(), 1, TIME, "_using_" + this.name(cropTag));
 
         this.makeRecipe(Fluids.WATER, 500, List.of(
                 ItemTagRegistry.ALCHEMICAL_SULFURS_GEMS_RARE,
-                ItemTagRegistry.SUGAR
-        ), SulfurRegistry.GEMS_RARE.get(), 1, TIME);
+                cropTag
+        ), SulfurRegistry.GEMS_RARE.get(), 1, TIME, "_using_" + this.name(cropTag));
 
         this.makeRecipe(Fluids.WATER, 1000, List.of(
                 ItemTagRegistry.ALCHEMICAL_SULFURS_GEMS_PRECIOUS,
-                ItemTagRegistry.SUGAR
-        ), SulfurRegistry.GEMS_PRECIOUS.get(), 1, TIME);
+                cropTag
+        ), SulfurRegistry.GEMS_PRECIOUS.get(), 1, TIME, "_using_" + this.name(cropTag));
+
+        this.makeRecipe(Fluids.WATER, 125, List.of(
+                ItemTagRegistry.ALCHEMICAL_SULFURS_METALS_ABUNDANT,
+                cropTag
+        ), SulfurRegistry.METALS_ABUNDANT.get(), 1, TIME, "_using_" + this.name(cropTag));
+
+        this.makeRecipe(Fluids.WATER, 250, List.of(
+                ItemTagRegistry.ALCHEMICAL_SULFURS_METALS_COMMON,
+                cropTag
+        ), SulfurRegistry.METALS_COMMON.get(), 1, TIME, "_using_" + this.name(cropTag));
+
+        this.makeRecipe(Fluids.WATER, 500, List.of(
+                ItemTagRegistry.ALCHEMICAL_SULFURS_METALS_RARE,
+                cropTag
+        ), SulfurRegistry.METALS_RARE.get(), 1, TIME, "_using_" + this.name(cropTag));
+
+        this.makeRecipe(Fluids.WATER, 1000, List.of(
+                ItemTagRegistry.ALCHEMICAL_SULFURS_METALS_PRECIOUS,
+                cropTag
+        ), SulfurRegistry.METALS_PRECIOUS.get(), 1, TIME, "_using_" + this.name(cropTag));
+
+        this.makeRecipe(Fluids.WATER, 125, List.of(
+                ItemTagRegistry.ALCHEMICAL_SULFURS_OTHER_MINERALS_ABUNDANT,
+                cropTag
+        ), SulfurRegistry.OTHER_MINERALS_ABUNDANT.get(), 1, TIME, "_using_" + this.name(cropTag));
+
+        this.makeRecipe(Fluids.WATER, 250, List.of(
+                ItemTagRegistry.ALCHEMICAL_SULFURS_OTHER_MINERALS_COMMON,
+                cropTag
+        ), SulfurRegistry.OTHER_MINERALS_COMMON.get(), 1, TIME, "_using_" + this.name(cropTag));
+
+        this.makeRecipe(Fluids.WATER, 500, List.of(
+                ItemTagRegistry.ALCHEMICAL_SULFURS_OTHER_MINERALS_RARE,
+                cropTag
+        ), SulfurRegistry.OTHER_MINERALS_RARE.get(), 1, TIME, "_using_" + this.name(cropTag));
+
+        this.makeRecipe(Fluids.WATER, 1000, List.of(
+                ItemTagRegistry.ALCHEMICAL_SULFURS_OTHER_MINERALS_PRECIOUS,
+                cropTag
+        ), SulfurRegistry.OTHER_MINERALS_PRECIOUS.get(), 1, TIME, "_using_" + this.name(cropTag));
     }
     public void makeRecipe(Fluid fluid, int fluidAmount, List<TagKey<Item>> ingredients, Item result, int resultCount, int time) {
         this.makeRecipe(this.name(result), fluid, fluidAmount, ingredients, result, resultCount, time);
     }
+
+    public void makeRecipe(Fluid fluid, int fluidAmount, List<TagKey<Item>> ingredients, Item result, int resultCount, int time, String postFix) {
+        this.makeRecipe(this.name(result) + postFix, fluid, fluidAmount, ingredients, result, resultCount, time);
+    }
+
     public void makeRecipe(String name, Fluid fluid, int fluidAmount, List<TagKey<Item>> ingredients, Item result, int resultCount, int time) {
         var recipe = this.makeRecipeJson(
                 this.makeFluidTagIngredient(this.locFor(fluid)),

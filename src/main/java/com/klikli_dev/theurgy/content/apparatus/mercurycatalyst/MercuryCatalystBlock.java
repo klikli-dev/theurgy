f5efd4834f2dad2ba0de70fd11ad9472904d16ec
Kli Kli <klikli@outlook.at>
2024-01-15 16:13:13 +0100

    fix: use behaviour of mercury catalyst

--- src/main/java/com/klikli_dev/theurgy/content/apparatus/mercurycatalyst/MercuryCatalystBlock.java
+++ src/main/java/com/klikli_dev/theurgy/content/apparatus/mercurycatalyst/MercuryCatalystBlock.java
@@ -33,12 +33,50 @@ public class MercuryCatalystBlock extends Block implements EntityBlock {
         this.itemHandlerBehaviour = new OneSlotItemHandlerBehaviour();
     }
 
+    public static int getBlockColor(BlockState pState, BlockAndTintGetter pLevel, BlockPos pPos, int pTintIndex) {
+        //tint index 0 is default and thus used by particles
+        //our model has 1
+        if (pTintIndex != 0 && pLevel != null && pPos != null && pLevel.getBlockEntity(pPos) instanceof MercuryCatalystBlockEntity blockEntity) {
+            var fillLevel = blockEntity.mercuryFluxStorage.getEnergyStored() / (float) blockEntity.mercuryFluxStorage.getMaxEnergyStored();
+
+            //if empty we return white, if full we should return blue: 0x0000FF
+            return getColorFromFillLevel(fillLevel);
+        }
+
+        return 0xFFFFFF;
+    }
+
+    public static int getItemColor(ItemStack pStack, int pTintIndex) {
+        if (pStack.hasTag() && pStack.getTag().contains("BlockEntityTag")) {
+            var fillLevel = pStack.getTag().getCompound("BlockEntityTag").getInt("mercuryFluxStorage") / (float) MercuryCatalystBlockEntity.CAPACITY;
+            return getColorFromFillLevel(fillLevel);
+        }
+
+        return 0xFFFFFF;
+    }
+
+    public static int getColorFromFillLevel(float fillLevel) {
+        int r = (int) ((1 - fillLevel) * 255.0);
+        int g = (int) ((1 - fillLevel) * 255.0);
+        int b = (int) (255.0); // b always stays the same
+
+        // Ensure the values are within 0-255 range
+        r = Math.max(0, Math.min(255, r));
+        g = Math.max(0, Math.min(255, g));
+        b = Math.max(0, Math.min(255, b));
+
+        // Combine the R, G, B values into a RGB integer
+        int rgb = (r << 16) | (g << 8) | b;
+
+        return rgb;
+    }
+
     @Override
     @SuppressWarnings("deprecation")
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
-        if (pLevel.isClientSide()) {
-            return InteractionResult.SUCCESS;
-        }
+        //We do not check for client side because
+        // a) returning success causes https://github.com/klikli-dev/theurgy/issues/158
+        // b) client side BEs are separate objects even in SP, so modification in our behaviours is safe
 
         if (this.itemHandlerBehaviour.useItemHandler(pState, pLevel, pPos, pPlayer, pHand, pHit) == InteractionResult.SUCCESS) {
             return InteractionResult.SUCCESS;
@@ -64,43 +102,5 @@ public class MercuryCatalystBlock extends Block implements EntityBlock {
                 blockEntity.tickServer();
             }
         };
-    }
-
-    public static int getBlockColor(BlockState pState, BlockAndTintGetter pLevel, BlockPos pPos, int pTintIndex){
-        //tint index 0 is default and thus used by particles
-        //our model has 1
-        if(pTintIndex != 0 && pLevel != null && pPos != null && pLevel.getBlockEntity(pPos) instanceof MercuryCatalystBlockEntity blockEntity){
-            var fillLevel = blockEntity.mercuryFluxStorage.getEnergyStored() / (float)  blockEntity.mercuryFluxStorage.getMaxEnergyStored();
-
-            //if empty we return white, if full we should return blue: 0x0000FF
-            return getColorFromFillLevel(fillLevel);
-        }
-
-        return 0xFFFFFF;
-    }
-
-    public static int getItemColor(ItemStack pStack, int pTintIndex){
-        if(pStack.hasTag() && pStack.getTag().contains("BlockEntityTag")) {
-            var fillLevel = pStack.getTag().getCompound("BlockEntityTag").getInt("mercuryFluxStorage") / (float) MercuryCatalystBlockEntity.CAPACITY;
-            return getColorFromFillLevel(fillLevel);
-        }
-
-        return 0xFFFFFF;
-    }
-
-    public static int getColorFromFillLevel(float fillLevel){
-        int r = (int) ((1 - fillLevel) * 255.0);
-        int g = (int) ((1 - fillLevel) * 255.0);
-        int b = (int) (255.0); // b always stays the same
-
-        // Ensure the values are within 0-255 range
-        r = Math.max(0, Math.min(255, r));
-        g = Math.max(0, Math.min(255, g));
-        b = Math.max(0, Math.min(255, b));
-
-        // Combine the R, G, B values into a RGB integer
-        int rgb = (r << 16) | (g << 8) | b;
-
-        return rgb;
     }
 }

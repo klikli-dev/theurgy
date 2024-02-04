c80e595c5450c9aca7fcf5f523710d3a244feea4
Kli Kli <klikli@outlook.at>
2024-02-04 16:13:15 +0100

    feat: introduce extraction rate

--- src/main/java/com/klikli_dev/theurgy/content/apparatus/logisticsitemconnector/extractor/LogisticsItemExtractorBehaviour.java
+++ src/main/java/com/klikli_dev/theurgy/content/apparatus/logisticsitemconnector/extractor/LogisticsItemExtractorBehaviour.java
@@ -8,6 +8,7 @@ import com.klikli_dev.theurgy.content.behaviour.logistics.ExtractorNodeBehaviour
 import com.klikli_dev.theurgy.content.behaviour.logistics.LeafNodeBehaviour;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -18,6 +19,12 @@ import net.neoforged.neoforge.items.ItemHandlerHelper;
 import org.jetbrains.annotations.Nullable;
 
 public class LogisticsItemExtractorBehaviour extends ExtractorNodeBehaviour<IItemHandler, @Nullable Direction> {
+
+    public static final int EXTRACTION_EVERY_N_TICKS = 20; // 1 second
+    public static final int MAX_EXTRACTION_AMOUNT = 10; //how many items to extract per extraction tick
+
+    public int extractionAmount = MAX_EXTRACTION_AMOUNT;
+
     public LogisticsItemExtractorBehaviour(BlockEntity blockEntity) {
         super(blockEntity, Capabilities.ItemHandler.BLOCK);
     }
@@ -48,12 +55,31 @@ public class LogisticsItemExtractorBehaviour extends ExtractorNodeBehaviour<IIte
     }
 
     @Override
+    public void saveAdditional(CompoundTag pTag) {
+        super.saveAdditional(pTag);
+
+        pTag.putInt("extractionAmount", this.extractionAmount);
+    }
+
+    @Override
+    public void load(CompoundTag pTag) {
+        super.load(pTag);
+
+        if (pTag.contains("extractionAmount")) {
+            this.extractionAmount = pTag.getInt("extractionAmount");
+        }
+    }
+
+    @Override
     public void tickServer() {
         //TODO: extract and insert nodes have to be disable-able
 
         //TODO: extraction should happen on a low tick, and in bulk.
 
         super.tickServer(); //this moves our distributor to the next target if needed.
+
+        if (this.blockEntity.getLevel().getGameTime() % EXTRACTION_EVERY_N_TICKS != 0) //slow tick
+            return;
 
         var insertTarget = this.distributor.target();
         if (insertTarget == null)
@@ -76,17 +102,25 @@ public class LogisticsItemExtractorBehaviour extends ExtractorNodeBehaviour<IIte
     }
 
     protected void performExtraction(IItemHandler extractCap, IItemHandler insertCap) {
-
-        //TODO: verify if this is the best way to transfer things with itemhandlers
         int extractSlot = getFirstMatchingSlot(extractCap);
         if (extractSlot == -1)
             return; //nothing found to extract
 
-        //first simulate extraction
-        var extractStack = extractCap.extractItem(extractSlot, 1, true);
+
+        //first simulate extraction, this tells us how much we can extract
+        var extractStack = extractCap.extractItem(extractSlot, this.extractionAmount, true);
         if (extractStack.isEmpty())
             return;
 
+
+        //TODO: better round robin distribution that works with extraction amounts > 1?
+        //  Simulate extract to get available
+        //  Simulate insert on each to get available space
+        //  Split evenly and take care of remainder
+        //  Then insert/extract
+        //  BUUUT: we should move that logic into the distributor
+        //  and: figure out if we do round robin only within a single tick, or if we do it across ticks
+        //      we probably need both depending on the transferred amount
 
         //and insertion
         ItemStack inserted = ItemHandlerHelper.insertItemStacked(insertCap, extractStack, true);

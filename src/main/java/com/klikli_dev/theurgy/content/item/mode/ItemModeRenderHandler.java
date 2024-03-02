ff9c061e2fd9af20f6c1558fe0cf353d91041d0e
Kli Kli <klikli@outlook.at>
2024-03-02 10:57:20 +0100

    chore: reuse compliance

--- src/main/java/com/klikli_dev/theurgy/content/item/mode/ItemModeRenderHandler.java
+++ src/main/java/com/klikli_dev/theurgy/content/item/mode/ItemModeRenderHandler.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 klikli-dev
+//
+// SPDX-License-Identifier: MIT
+
 package com.klikli_dev.theurgy.content.item.mode;
 
 import net.neoforged.neoforge.client.event.RenderHighlightEvent;
--- src/main/java/com/klikli_dev/theurgy/content/item/mode/ModeItem.java
+++ src/main/java/com/klikli_dev/theurgy/content/item/mode/ModeItem.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 klikli-dev
+//
+// SPDX-License-Identifier: MIT
+
 package com.klikli_dev.theurgy.content.item.mode;
 
 import com.klikli_dev.theurgy.network.Networking;

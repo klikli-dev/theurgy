8be64b2e608fa46a602afe3cb00f452b509bdde0
Kli Kli <klikli@outlook.at>
2024-05-26 17:16:38 +0200

    feat: update modo pages

--- src/main/java/com/klikli_dev/theurgy/integration/modonomicon/page/liquefaction/BookLiquefactionRecipePage.java
+++ src/main/java/com/klikli_dev/theurgy/integration/modonomicon/page/liquefaction/BookLiquefactionRecipePage.java
@@ -13,7 +13,8 @@ import com.klikli_dev.modonomicon.book.page.BookRecipePage;
 import com.klikli_dev.theurgy.content.recipe.LiquefactionRecipe;
 import com.klikli_dev.theurgy.integration.modonomicon.TheurgyModonomiconConstants;
 import com.klikli_dev.theurgy.registry.RecipeTypeRegistry;
-import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.GsonHelper;
 
@@ -23,16 +24,16 @@ public class BookLiquefactionRecipePage extends BookProcessingRecipePage<Liquefa
         super(RecipeTypeRegistry.LIQUEFACTION.get(), title1, recipeId1, title2, recipeId2, text, anchor, condition);
     }
 
-    public static BookLiquefactionRecipePage fromJson(JsonObject json) {
-        var common = BookRecipePage.commonFromJson(json);
+    public static BookLiquefactionRecipePage fromJson(JsonObject json, HolderLookup.Provider provider) {
+        var common = BookRecipePage.commonFromJson(json, provider);
         var anchor = GsonHelper.getAsString(json, "anchor", "");
         var condition = json.has("condition")
-                ? BookCondition.fromJson(json.getAsJsonObject("condition"))
+                ? BookCondition.fromJson(json.getAsJsonObject("condition"), provider)
                 : new BookNoneCondition();
         return new BookLiquefactionRecipePage(common.title1(), common.recipeId1(), common.title2(), common.recipeId2(), common.text(), anchor, condition);
     }
 
-    public static BookLiquefactionRecipePage fromNetwork(FriendlyByteBuf buffer) {
+    public static BookLiquefactionRecipePage fromNetwork(RegistryFriendlyByteBuf buffer) {
         var common = BookRecipePage.commonFromNetwork(buffer);
         var anchor = buffer.readUtf();
         var condition = BookCondition.fromNetwork(buffer);

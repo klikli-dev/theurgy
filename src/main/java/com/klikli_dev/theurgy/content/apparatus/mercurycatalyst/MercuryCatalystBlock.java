fc3623dfad76ea68e3a10e817679d8c04aa78814
Kli Kli <klikli@outlook.at>
2024-05-27 15:59:30 +0200

    feat: use data components

--- src/main/java/com/klikli_dev/theurgy/content/apparatus/mercurycatalyst/MercuryCatalystBlock.java
+++ src/main/java/com/klikli_dev/theurgy/content/apparatus/mercurycatalyst/MercuryCatalystBlock.java
@@ -7,17 +7,15 @@ package com.klikli_dev.theurgy.content.apparatus.mercurycatalyst;
 import com.klikli_dev.theurgy.content.behaviour.itemhandler.ItemHandlerBehaviour;
 import com.klikli_dev.theurgy.content.behaviour.itemhandler.OneSlotItemHandlerBehaviour;
 import com.klikli_dev.theurgy.registry.BlockEntityRegistry;
+import com.klikli_dev.theurgy.registry.DataComponentRegistry;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -58,8 +56,8 @@ public class MercuryCatalystBlock extends Block implements EntityBlock {
     }
 
     public static int getItemColor(ItemStack pStack, int pTintIndex) {
-        if (pStack.hasTag() && pStack.getTag().contains("BlockEntityTag")) {
-            var fillLevel = pStack.getTag().getCompound("BlockEntityTag").getInt("mercuryFluxStorage") / (float) MercuryCatalystBlockEntity.CAPACITY;
+        if (pStack.has(DataComponentRegistry.MERCURY_FLUX_STORAGE.get())) {
+            var fillLevel = pStack.get(DataComponentRegistry.MERCURY_FLUX_STORAGE.get()) / (float) MercuryCatalystBlockEntity.CAPACITY;
             return getColorFromFillLevel(fillLevel);
         }
 
@@ -94,7 +92,6 @@ public class MercuryCatalystBlock extends Block implements EntityBlock {
 
         return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
     }
-
 
 
     @Nullable

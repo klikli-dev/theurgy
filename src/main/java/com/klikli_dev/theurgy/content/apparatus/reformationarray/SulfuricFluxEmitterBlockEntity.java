c46f7ce03e188c8674fd3ce39ed8c7ca5f5e2198
Kli Kli <klikli@outlook.at>
2024-06-16 08:53:50 +0200

    fix: crash when switching reformation array blocks
    The crash would also occur if any other block entity is placed where a reformation block was.
    
    Closes #184

--- src/main/java/com/klikli_dev/theurgy/content/apparatus/reformationarray/SulfuricFluxEmitterBlockEntity.java
+++ src/main/java/com/klikli_dev/theurgy/content/apparatus/reformationarray/SulfuricFluxEmitterBlockEntity.java
@@ -4,41 +4,31 @@
 
 package com.klikli_dev.theurgy.content.apparatus.reformationarray;
 
-import com.klikli_dev.theurgy.TheurgyConstants;
 import com.klikli_dev.theurgy.content.behaviour.CraftingBehaviour;
 import com.klikli_dev.theurgy.content.behaviour.selection.SelectionBehaviour;
 import com.klikli_dev.theurgy.content.capability.DefaultMercuryFluxStorage;
 import com.klikli_dev.theurgy.content.entity.FollowProjectile;
 import com.klikli_dev.theurgy.content.recipe.wrapper.ReformationArrayRecipeWrapper;
 import com.klikli_dev.theurgy.content.render.Color;
-import com.klikli_dev.theurgy.content.render.outliner.Outliner;
 import com.klikli_dev.theurgy.registry.BlockEntityRegistry;
 import com.klikli_dev.theurgy.registry.BlockRegistry;
 import com.klikli_dev.theurgy.registry.CapabilityRegistry;
 import com.klikli_dev.theurgy.util.EntityUtil;
 import com.mojang.datafixers.util.Pair;
 import io.netty.handler.codec.EncoderException;
-import net.minecraft.ChatFormatting;
 import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtOps;
 import net.minecraft.network.Connection;
-import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.shapes.Shapes;
-import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.IItemHandlerModifiable;
@@ -122,8 +112,8 @@ public class SulfuricFluxEmitterBlockEntity extends BlockEntity {
         this.isValidMultiblock = true; //set to true, then set to false if any of the checks fail
 
         if (this.targetPedestal != null) {
-            var targetPedestalBlockEntity = (ReformationTargetPedestalBlockEntity) this.level.getBlockEntity(this.targetPedestal.getBlockPos());
-            if (targetPedestalBlockEntity == null) {
+            var targetPedestalBlockEntity = this.level.getBlockEntity(this.targetPedestal.getBlockPos());
+            if (!(targetPedestalBlockEntity instanceof ReformationTargetPedestalBlockEntity)) {
                 this.isValidMultiblock = false;
             }
         } else {
@@ -131,8 +121,8 @@ public class SulfuricFluxEmitterBlockEntity extends BlockEntity {
         }
 
         if (this.resultPedestal != null) {
-            var resultPedestalBlockEntity = (ReformationResultPedestalBlockEntity) this.level.getBlockEntity(this.resultPedestal.getBlockPos());
-            if (resultPedestalBlockEntity == null) {
+            var resultPedestalBlockEntity = this.level.getBlockEntity(this.resultPedestal.getBlockPos());
+            if (!(resultPedestalBlockEntity instanceof ReformationResultPedestalBlockEntity)) {
                 this.isValidMultiblock = false;
             }
         } else {
@@ -141,8 +131,8 @@ public class SulfuricFluxEmitterBlockEntity extends BlockEntity {
 
         var hasSourcePedestals = false;
         for (var sourcePedestal : this.sourcePedestals) {
-            var sourcePedestalBlockEntity = (ReformationSourcePedestalBlockEntity) this.level.getBlockEntity(sourcePedestal.getBlockPos());
-            if (sourcePedestalBlockEntity != null) {
+            var sourcePedestalBlockEntity = this.level.getBlockEntity(sourcePedestal.getBlockPos());
+            if (!(sourcePedestalBlockEntity instanceof ReformationSourcePedestalBlockEntity)) {
                 hasSourcePedestals = true;
             }
         }

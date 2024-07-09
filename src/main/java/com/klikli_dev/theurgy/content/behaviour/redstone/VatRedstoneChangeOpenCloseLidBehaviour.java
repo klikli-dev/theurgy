cf49ad56fec48196caf7e8b5d6ebbfbc5b14753e
Kli Kli <klikli@outlook.at>
2024-07-09 13:02:18 +0200

    chore: re-establish reuse compliance

--- src/main/java/com/klikli_dev/theurgy/content/behaviour/redstone/VatRedstoneChangeOpenCloseLidBehaviour.java
+++ src/main/java/com/klikli_dev/theurgy/content/behaviour/redstone/VatRedstoneChangeOpenCloseLidBehaviour.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 klikli-dev
+//
+// SPDX-License-Identifier: MIT
+
 package com.klikli_dev.theurgy.content.behaviour.redstone;
 
 import com.klikli_dev.theurgy.content.behaviour.crafting.HasCraftingBehaviour;
--- src/main/java/com/klikli_dev/theurgy/content/behaviour/storage/OutputStorageBehaviour.java
+++ src/main/java/com/klikli_dev/theurgy/content/behaviour/storage/OutputStorageBehaviour.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 klikli-dev
+//
+// SPDX-License-Identifier: MIT
+
 package com.klikli_dev.theurgy.content.behaviour.storage;
 
 import net.neoforged.neoforge.items.IItemHandler;

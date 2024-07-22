ea9e3567c5b12fc51f970b939f960d772cb02a68
Kli Kli <klikli@outlook.at>
2024-07-22 16:27:24 +0200

    chore: work on reformation recipe renderer for emi

--- src/main/java/com/klikli_dev/theurgy/integration/emi/ReformationEmiRecipe.java
+++ src/main/java/com/klikli_dev/theurgy/integration/emi/ReformationEmiRecipe.java
@@ -1,6 +1,8 @@
 package com.klikli_dev.theurgy.integration.emi;
 
+import com.klikli_dev.theurgy.TheurgyConstants;
 import com.klikli_dev.theurgy.content.recipe.ReformationRecipe;
+import com.klikli_dev.theurgy.integration.jei.JeiDrawables;
 import com.klikli_dev.theurgy.registry.ItemRegistry;
 import dev.emi.emi.api.recipe.EmiRecipe;
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
@@ -8,13 +10,20 @@ import dev.emi.emi.api.render.EmiTexture;
 import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.api.widget.WidgetHolder;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.component.DataComponentPatch;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.RecipeHolder;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static mezz.jei.api.recipe.RecipeIngredientRole.INPUT;
 
 public class ReformationEmiRecipe implements EmiRecipe {
 
@@ -69,37 +78,49 @@ public class ReformationEmiRecipe implements EmiRecipe {
     @Override
     public void addWidgets(WidgetHolder widgets) {
         widgets.addTexture(EmiTexture.EMPTY_ARROW, 130, 19);
+        widgets.addTexture(EmiTexture.EMPTY_ARROW, 19, 19);
+        widgets.addTexture(EmiTexture.EMPTY_ARROW, 65, 19);
 
-        widgets.addSlot(EmiStack.of(ItemRegistry.SULFURIC_FLUX_EMITTER.get()), 1, 15).catalyst(true).drawBack(false);
+        widgets.addSlot(EmiStack.of(ItemRegistry.SULFURIC_FLUX_EMITTER.get()), 1, 19).drawBack(false);
 
-        //TODO arrow
-        //TODO second arrow
+        widgets.addSlot(EmiStack.of(Items.BARRIER, DataComponentPatch.builder().set(
+                        DataComponents.ITEM_NAME, Component.translatable(TheurgyConstants.I18n.JEI.TARGET_SULFUR_TOOLTIP).withStyle(s -> s.withItalic(true).withColor(ChatFormatting.WHITE))).build())
+                , 45, 1).drawBack(false)
+                .appendTooltip(Component.translatable(TheurgyConstants.I18n.JEI.TARGET_SULFUR_TOOLTIP).withStyle(ChatFormatting.ITALIC))
+        ;
 
         widgets.addSlot(EmiIngredient.of(this.recipe.value().getTarget()), 45, 19).catalyst(true);
 
-        widgets.addSlot(EmiStack.of(ItemRegistry.REFORMATION_TARGET_PEDESTAL.get()), 45, 35).catalyst(true).drawBack(false);
+
+        widgets.addSlot(EmiStack.of(ItemRegistry.REFORMATION_TARGET_PEDESTAL.get()), 45, 42).drawBack(false);
+
 
         int sourceSlotX = 90;
         int startY = 55;
         int sourceSlotY = startY; // Start from the bottom
-        for (int i = 0; i < this.recipe.value().getSources().size(); i++) {
-            widgets.addSlot(EmiIngredient.of(this.recipe.value().getSources().get(i)), sourceSlotX, sourceSlotY);
+
+        for (int i = 0; i < 8; i++) {
+            if (i < recipe.value().getSources().size()){
+                widgets.addSlot(EmiIngredient.of(this.recipe.value().getSources().get(i)), sourceSlotX, sourceSlotY);
+            } else {
+                widgets.addSlot(sourceSlotX, sourceSlotY);
+            }
 
             sourceSlotY -= 18; // Move upwards
             if (i % 4 == 3) {
                 sourceSlotY = startY; // Reset to the bottom
                 sourceSlotX += 18; // Move to the right
             }
-
-            //TODO: draw empty slots
         }
 
         //TODO: Draw mercury flux
 
-        widgets.addSlot(EmiStack.of(ItemRegistry.REFORMATION_SOURCE_PEDESTAL.get()), 90 + 9, startY + 18).catalyst(true).drawBack(false);
+        //TODO: draw source pedestal count
+
+        widgets.addSlot(EmiStack.of(ItemRegistry.REFORMATION_SOURCE_PEDESTAL.get()), 90 + 9, startY + 18 + 5).drawBack(false);
 
         widgets.addSlot(EmiStack.of(this.recipe.value().getResultItem(RegistryAccess.EMPTY)), 160, 19).recipeContext(this);
 
-        widgets.addSlot(EmiStack.of(ItemRegistry.REFORMATION_RESULT_PEDESTAL.get()), 160, 42).catalyst(true).drawBack(false);
+        widgets.addSlot(EmiStack.of(ItemRegistry.REFORMATION_RESULT_PEDESTAL.get()), 160, 42).drawBack(false);
     }
 }

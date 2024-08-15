ea69f68e1c43181c9a4e9e80cd2717b0537c65cc
Kli Kli <klikli@outlook.at>
2024-08-15 20:07:52 +0200

    fix: don't use wither skeleton skull in niter as the icon does not render in hotbar

--- src/main/java/com/klikli_dev/theurgy/registry/NiterRegistry.java
+++ src/main/java/com/klikli_dev/theurgy/registry/NiterRegistry.java
@@ -45,7 +45,7 @@ public class NiterRegistry {
 
     public static final DeferredItem<AlchemicalNiterItem> MOBS_ABUNDANT = register("mobs_abundant", Items.ROTTEN_FLESH, AlchemicalDerivativeTier.ABUNDANT);
     public static final DeferredItem<AlchemicalNiterItem> MOBS_COMMON = register("mobs_commond", Items.BLAZE_ROD, AlchemicalDerivativeTier.COMMON);
-    public static final DeferredItem<AlchemicalNiterItem> MOBS_RARE = register("mobs_rare", Items.WITHER_SKELETON_SKULL, AlchemicalDerivativeTier.RARE);
+    public static final DeferredItem<AlchemicalNiterItem> MOBS_RARE = register("mobs_rare", Items.GHAST_TEAR, AlchemicalDerivativeTier.RARE);
     public static final DeferredItem<AlchemicalNiterItem> MOBS_PRECIOUS = register("mobs_precious", Items.NETHER_STAR, AlchemicalDerivativeTier.PRECIOUS);
 
     public static DeferredItem<AlchemicalNiterItem> register(String name, TagKey<Item> source, AlchemicalDerivativeTier tier) {

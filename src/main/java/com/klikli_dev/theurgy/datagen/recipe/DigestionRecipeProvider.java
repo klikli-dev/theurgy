399dc0ef0fd47ab0d2baafc8336cbb71b5eb5b8f
Kli Kli <klikli@outlook.at>
2026-01-11 18:08:50 +0100

    refactor: Correct Items import in data generator
    Fix import statement for Minecraft's Items class in DigestionRecipeProvider.

--- src/main/java/com/klikli_dev/theurgy/datagen/recipe/DigestionRecipeProvider.java
+++ src/main/java/com/klikli_dev/theurgy/datagen/recipe/DigestionRecipeProvider.java
@@ -18,7 +18,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.items.Items;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.material.Fluid;

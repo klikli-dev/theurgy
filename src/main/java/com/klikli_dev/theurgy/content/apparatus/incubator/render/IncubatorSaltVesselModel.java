9070864fad3369661ea89902c5ebd74d5563c47f
Kli Kli <klikli@outlook.at>
2026-01-12 07:20:59 +0100

    fix(render): update Geckolib models and shader config for 1.21.3
    Updated Geckolib GeoModel method signatures to match version 4.7.3. Updated rendertype_distance_lines.json to correctly reference custom shaders in theurgy:core namespace.

--- src/main/java/com/klikli_dev/theurgy/content/apparatus/incubator/render/IncubatorSaltVesselModel.java
+++ src/main/java/com/klikli_dev/theurgy/content/apparatus/incubator/render/IncubatorSaltVesselModel.java
@@ -8,15 +8,16 @@ import com.klikli_dev.theurgy.Theurgy;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.animatable.GeoAnimatable;
 import software.bernie.geckolib.model.GeoModel;
+import software.bernie.geckolib.renderer.GeoRenderer;
 
 public class IncubatorSaltVesselModel extends GeoModel {
     @Override
-    public ResourceLocation getModelResource(GeoAnimatable animatable) {
+    public ResourceLocation getModelResource(GeoAnimatable animatable, GeoRenderer renderer) {
         return Theurgy.loc("geo/incubator_vessel.geo.json");
     }
 
     @Override
-    public ResourceLocation getTextureResource(GeoAnimatable animatable) {
+    public ResourceLocation getTextureResource(GeoAnimatable animatable, GeoRenderer renderer) {
         return Theurgy.loc("textures/block/incubator_vessel_bronze.png");
     }
 

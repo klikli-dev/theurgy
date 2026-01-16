b51b9f48fdfe086317ed1bbe5159d1be163f184d
Kli Kli <klikli@outlook.at>
2026-01-16 22:11:23 +0100

    Refactor: Migrate particle rendering and render states
    Migrates particle render types to modern API and updates existing render states.

--- src/main/java/com/klikli_dev/theurgy/content/particle/ParticleRenderTypes.java
+++ src/main/java/com/klikli_dev/theurgy/content/particle/ParticleRenderTypes.java
@@ -6,57 +6,10 @@
 package com.klikli_dev.theurgy.content.particle;
 
 import com.klikli_dev.theurgy.Theurgy;
-import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.BufferBuilder;
-import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import com.mojang.blaze3d.vertex.Tesselator;
-import com.mojang.blaze3d.vertex.VertexFormat;
-import net.minecraft.client.Minecraft;
+import com.klikli_dev.theurgy.content.render.RenderTypes;
 import net.minecraft.client.particle.ParticleRenderType;
-import net.minecraft.client.renderer.texture.TextureAtlas;
-import net.minecraft.client.renderer.texture.TextureManager;
-import org.jetbrains.annotations.Nullable;
 
 public class ParticleRenderTypes {
-    public static final ParticleRenderType EMBER_RENDER = new ParticleRenderType() {
-        @Nullable
-        @Override
-        public BufferBuilder begin(Tesselator tesselator, TextureManager p_107437_) {
-            Minecraft.getInstance().gameRenderer.lightTexture().turnOnLightLayer();
-            RenderSystem.enableBlend();
-            RenderSystem.enableCull();
-            RenderSystem.setShaderTexture(0, TextureAtlas.LOCATION_PARTICLES);
-            RenderSystem.enableDepthTest();
-            RenderSystem.depthMask(false);
-            RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA.value, GlStateManager.DestFactor.ONE.value);
-            return tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.PARTICLE);
-        }
-
-        @Override
-        public String toString() {
-            return Theurgy.MODID + ":em_rend";
-        }
-    };
-
-    public static final ParticleRenderType EMBER_RENDER_NO_MASK = new ParticleRenderType() {
-        @Override
-        @SuppressWarnings("deprecation")
-        public BufferBuilder begin(Tesselator tesselator, TextureManager textureManager) {
-            RenderSystem.disableDepthTest();
-            RenderSystem.enableBlend();
-
-            RenderSystem.enableCull();
-
-            RenderSystem.setShaderTexture(0, TextureAtlas.LOCATION_PARTICLES);
-            RenderSystem.depthMask(false);
-            RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA.value, GlStateManager.DestFactor.ONE.value);
-            return tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.PARTICLE);
-        }
-        
-        @Override
-        public String toString() {
-            return Theurgy.MODID + ":em_rend_no_mask";
-        }
-    };
+    public static final ParticleRenderType EMBER_RENDER = new ParticleRenderType(Theurgy.MODID + ":em_rend", RenderTypes.EMBER);
+    public static final ParticleRenderType EMBER_RENDER_NO_MASK = new ParticleRenderType(Theurgy.MODID + ":em_rend_no_mask", RenderTypes.EMBER_NO_MASK);
 }

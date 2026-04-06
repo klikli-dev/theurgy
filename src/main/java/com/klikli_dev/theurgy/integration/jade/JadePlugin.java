96e5a80a723f239455eb35816e15f91649da2766
Kli Kli <klikli@outlook.at>
2026-04-06 13:23:21 +0200

    feat(jade): update mercury flux provider to support jade 26.1 API structure

--- src/main/java/com/klikli_dev/theurgy/integration/jade/JadePlugin.java
+++ src/main/java/com/klikli_dev/theurgy/integration/jade/JadePlugin.java
@@ -4,21 +4,29 @@
 
 package com.klikli_dev.theurgy.integration.jade;
 
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import snownee.jade.api.IWailaClientRegistration;
 import snownee.jade.api.IWailaCommonRegistration;
 import snownee.jade.api.IWailaPlugin;
 import snownee.jade.api.WailaPlugin;
+import snownee.jade.api.view.IServerExtensionProvider;
+import snownee.jade.api.view.IClientExtensionProvider;
+import snownee.jade.api.view.EnergyView;
+import net.minecraft.nbt.CompoundTag;
 
 @WailaPlugin
 public class JadePlugin implements IWailaPlugin {
     @Override
+    @SuppressWarnings("unchecked")
     public void register(IWailaCommonRegistration registration) {
-        registration.registerEnergyStorage(MercuryFluxEnergyProvider.get(), BlockEntity.class);
+        registration.registerEnergyStorage((IServerExtensionProvider) (Object) MercuryFluxEnergyProvider.get(), BlockEntity.class);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public void registerClient(IWailaClientRegistration registration) {
-        registration.registerEnergyStorageClient(MercuryFluxEnergyProvider.get());
+        registration.registerBlockComponent(MercuryFluxEnergyProvider.Client.INSTANCE, Block.class);
+        registration.registerEnergyStorageClient((IClientExtensionProvider) (Object) MercuryFluxEnergyProvider.get());
     }
 }
--- src/main/java/com/klikli_dev/theurgy/integration/jade/MercuryFluxEnergyProvider.java
+++ src/main/java/com/klikli_dev/theurgy/integration/jade/MercuryFluxEnergyProvider.java
@@ -6,12 +6,17 @@ package com.klikli_dev.theurgy.integration.jade;
 
 import com.klikli_dev.theurgy.Theurgy;
 import com.klikli_dev.theurgy.registry.CapabilityRegistry;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import org.jetbrains.annotations.Nullable;
 import snownee.jade.api.Accessor;
+import snownee.jade.api.IBlockComponentProvider;
+import snownee.jade.api.ITooltip;
+import snownee.jade.api.BlockAccessor;
+import snownee.jade.api.config.IPluginConfig;
 import snownee.jade.api.view.*;
-import snownee.jade.util.CommonProxy;
 
 import java.util.List;
 import java.util.Objects;
@@ -25,26 +30,31 @@ public class MercuryFluxEnergyProvider implements IServerExtensionProvider<Compo
         return instance;
     }
 
-    public static @Nullable List<ViewGroup<CompoundTag>> wrapMercuryFluxStorage(Accessor<?> accessor) {
-        var storage = CommonProxy.getDefaultStorage(accessor, CapabilityRegistry.MERCURY_FLUX_HANDLER, null);
+    public static @Nullable List<ViewGroup<CompoundTag>> wrapMercuryFluxStorage(BlockEntity blockEntity) {
+        var storage = blockEntity.getLevel().getCapability(CapabilityRegistry.MERCURY_FLUX_HANDLER, blockEntity.getBlockPos(), blockEntity.getBlockState(), blockEntity, null);
         if (storage != null) {
-            ViewGroup<CompoundTag> group = new ViewGroup(List.of(EnergyView.of(storage.getEnergyStored(), storage.getMaxEnergyStored())));
-            group.getExtraData().putString("Unit", "MF");
+            CompoundTag tag = new CompoundTag();
+            tag.putLong("cur", storage.getEnergyStored());
+            tag.putLong("max", storage.getMaxEnergyStored());
+            tag.putString("unit", "MF");
+            ViewGroup<CompoundTag> group = new ViewGroup<>(List.of(tag));
             return List.of(group);
         } else {
             return null;
         }
     }
 
-    public static boolean hasDefaultMercuryFluxStorage(Accessor<?> accessor) {
-        return CommonProxy.hasDefaultStorage(accessor, CapabilityRegistry.MERCURY_FLUX_HANDLER, null);
-    }
-
     @Override
+    @SuppressWarnings("unchecked")
     public List<ClientViewGroup<EnergyView>> getClientGroups(Accessor<?> accessor, List<ViewGroup<CompoundTag>> groups) {
         return groups.stream().map($ -> {
-            String unit = $.getExtraData().getString("Unit");
-            return new ClientViewGroup<>($.views.stream().map(tag -> EnergyView.read(tag, unit)).filter(Objects::nonNull).toList());
+            List<EnergyView> views = $.views.stream().map(tag -> {
+                long cur = tag.getLong("cur").orElse(0L);
+                long max = tag.getLong("max").orElse(0L);
+                String unit = tag.getString("unit").orElse("MF");
+                return EnergyView.read(new EnergyView.Data(cur, max), unit);
+            }).filter(Objects::nonNull).toList();
+            return new ClientViewGroup<>(views);
         }).toList();
     }
 
@@ -55,10 +65,22 @@ public class MercuryFluxEnergyProvider implements IServerExtensionProvider<Compo
 
     @Override
     public @Nullable List<ViewGroup<CompoundTag>> getGroups(Accessor<?> accessor) {
-        return wrapMercuryFluxStorage(accessor);
+        if (accessor instanceof BlockAccessor blockAccessor) {
+            return wrapMercuryFluxStorage(blockAccessor.getBlockEntity());
+        }
+        return null;
     }
 
-    public boolean shouldRequestData(Accessor<?> accessor) {
-        return hasDefaultMercuryFluxStorage(accessor);
+    public enum Client implements IBlockComponentProvider {
+        INSTANCE;
+
+        @Override
+        public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
+        }
+
+        @Override
+        public Identifier getUid() {
+            return ID;
+        }
     }
 }

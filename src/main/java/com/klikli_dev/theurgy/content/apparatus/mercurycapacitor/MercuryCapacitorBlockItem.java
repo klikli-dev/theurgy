7f53b719a2a4d271fe3366b575ac190a48fef300
Kli Kli <klikli@outlook.at>
2026-04-14 20:18:18 +0200

    feat: Add MercuryCatalystBlockItem and MercuryCapacitorBlockItem classes

---
+++ src/main/java/com/klikli_dev/theurgy/content/apparatus/mercurycapacitor/MercuryCapacitorBlockItem.java
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2024 klikli-dev
+//
+// SPDX-License-Identifier: MIT
+
+package com.klikli_dev.theurgy.content.apparatus.mercurycapacitor;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
+
+public class MercuryCapacitorBlockItem extends BlockItem {
+
+    public MercuryCapacitorBlockItem(Block pBlock, Properties pProperties) {
+        super(pBlock, pProperties);
+    }
+}
---
+++ src/main/java/com/klikli_dev/theurgy/content/apparatus/mercurycatalyst/MercuryCatalystBlockItem.java
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2024 klikli-dev
+//
+// SPDX-License-Identifier: MIT
+
+package com.klikli_dev.theurgy.content.apparatus.mercurycatalyst;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
+
+public class MercuryCatalystBlockItem extends BlockItem {
+
+    public MercuryCatalystBlockItem(Block pBlock, Properties pProperties) {
+        super(pBlock, pProperties);
+    }
+}

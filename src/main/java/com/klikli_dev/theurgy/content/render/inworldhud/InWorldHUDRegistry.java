645c0fd60adae92e970dc1650ee26e0b0ba99ef4
Kli Kli <klikli@outlook.at>
2026-04-14 19:53:49 +0200

    feat: Add in-world HUD for Mercury Capacitor showing flux level

--- src/main/java/com/klikli_dev/theurgy/content/render/inworldhud/InWorldHUDRegistry.java
+++ src/main/java/com/klikli_dev/theurgy/content/render/inworldhud/InWorldHUDRegistry.java
@@ -8,6 +8,7 @@ import com.klikli_dev.theurgy.content.render.inworldhud.provider.BlockTitleInWor
 import com.klikli_dev.theurgy.content.render.inworldhud.provider.FluidStorageInWorldHUDProvider;
 import com.klikli_dev.theurgy.content.render.inworldhud.provider.GenericCraftingProgressInWorldHUDProvider;
 import com.klikli_dev.theurgy.content.render.inworldhud.provider.ItemStorageInWorldHUDProvider;
+import com.klikli_dev.theurgy.content.render.inworldhud.provider.MercuryCapacitorInWorldHUDProvider;
 import com.klikli_dev.theurgy.content.render.inworldhud.provider.MercuryFluxStorageInWorldHUDProvider;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
@@ -39,6 +40,8 @@ public class InWorldHUDRegistry {
         registerGenericProvider(new MercuryFluxStorageInWorldHUDProvider());
         registerGenericProvider(new FluidStorageInWorldHUDProvider());
         registerGenericProvider(new ItemStorageInWorldHUDProvider());
+
+        registerBlockProvider(MercuryCapacitorInWorldHUDProvider.appliesBlock(), new MercuryCapacitorInWorldHUDProvider());
 
         defaultsRegistered = true;
     }

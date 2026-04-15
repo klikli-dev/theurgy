125494b0feae06171141e97fc3230e5671745999
Kli Kli <klikli@outlook.at>
2026-04-15 09:03:58 +0200

    fix: Enable disableDepthTest for reformation pedestal glow particles
    The pedestals were using createOptions(color, size, alpha, age) which
    hardcodes disableDepthTest=false. Updated to use createOptions with
    disableDepthTest=true to prevent z-fighting on overlapping particles.

--- src/main/java/com/klikli_dev/theurgy/content/apparatus/reformationarray/ReformationTargetPedestalBlockEntity.java
+++ src/main/java/com/klikli_dev/theurgy/content/apparatus/reformationarray/ReformationTargetPedestalBlockEntity.java
@@ -54,6 +54,7 @@ public class ReformationTargetPedestalBlockEntity extends BlockEntity implements
             var pos = this.getBlockPos();
             this.level.addParticle(GlowParticleProvider.createOptions(
                     ParticleColor.fromInt(0x0000FF),
+                    true,
                     0.5f,
                     0.75f,
                     200), pos.getX() + 0.5f, pos.getY() + 1.0f, pos.getZ() + 0.5f, 0, 0, 0);

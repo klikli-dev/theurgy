3a399e4089860a8007ca4634bddf57dd6a52c0d1
Kli Kli <klikli@outlook.at>
2026-04-18 14:16:10 +0200

    fix: restore pedestal item validation

--- src/main/java/com/klikli_dev/theurgy/content/storage/SettableItemStorage.java
+++ src/main/java/com/klikli_dev/theurgy/content/storage/SettableItemStorage.java
@@ -35,8 +35,13 @@ public interface SettableItemStorage extends ResourceHandler<ItemResource> {
         return ItemStorageHelper.getSlotLimit(this, slot);
     }
 
+    @Override
+    default boolean isValid(int slot, ItemResource resource) {
+        return resource.isEmpty() || this.isItemValid(slot, resource.toStack(1));
+    }
+
     default boolean isItemValid(int slot, ItemStack stack) {
-        return ItemStorageHelper.isItemValid(this, slot, stack);
+        return true;
     }
 }
 

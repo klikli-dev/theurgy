a366585a43b36ccb9b7c6ab79651d2307bf4d1d4
Kli Kli <klikli@outlook.at>
2026-04-29 23:02:42 +0200

    feat: use cdg top background in attribute filter

--- src/main/java/com/klikli_dev/theurgy/content/item/filter/AttributeFilterScreen.java
+++ src/main/java/com/klikli_dev/theurgy/content/item/filter/AttributeFilterScreen.java
@@ -4,8 +4,8 @@
 
 package com.klikli_dev.theurgy.content.item.filter;
 
+import com.klikli_dev.codedefinedgui.gui.texture.GuiSprites;
 import com.klikli_dev.codedefinedgui.gui.widget.GuiBackgroundWidget;
-import com.klikli_dev.theurgy.content.gui.GuiSprites;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
 
@@ -14,7 +14,7 @@ public class AttributeFilterScreen extends TheurgyAttributeFilterScreenBase {
     private static final int TOP_SECTION_HEIGHT = 15;
     private static final int MIDDLE_SECTION_HEIGHT = 34;
     private static final int BOTTOM_SECTION_HEIGHT = FILTER_BACKGROUND_HEIGHT - TOP_SECTION_HEIGHT - MIDDLE_SECTION_HEIGHT;
-    private static final int BLUE_TINT = 0xFFB7D1FF;
+    private static final int LIGHT_BLUE_TINT = 0xFFD9E8FF;
     private static final int DARK_TINT = 0xFFB0B0B0;
 
     public AttributeFilterScreen(AttributeFilterMenu menu, Inventory playerInventory, Component title) {
@@ -23,8 +23,8 @@ public class AttributeFilterScreen extends TheurgyAttributeFilterScreenBase {
 
     @Override
     protected void addBackgroundWidgets() {
-        this.root.addChild(new GuiBackgroundWidget(this, this.leftPos, this.topPos, this.imageWidth, TOP_SECTION_HEIGHT, GuiSprites.GUI_BACKGROUND.tinted(BLUE_TINT)));
-        this.root.addChild(new GuiBackgroundWidget(this, this.leftPos + 3, this.topPos + TOP_SECTION_HEIGHT, this.imageWidth - 6, MIDDLE_SECTION_HEIGHT, GuiSprites.GUI_BACKGROUND));
-        this.root.addChild(new GuiBackgroundWidget(this, this.leftPos, this.topPos + TOP_SECTION_HEIGHT + MIDDLE_SECTION_HEIGHT, this.imageWidth, BOTTOM_SECTION_HEIGHT, GuiSprites.GUI_BACKGROUND.tinted(DARK_TINT)));
+        this.root.addChild(new GuiBackgroundWidget(this, this.leftPos, this.topPos, this.imageWidth, TOP_SECTION_HEIGHT, GuiSprites.GUI_BACKGROUND.tinted(LIGHT_BLUE_TINT)));
+        this.root.addChild(new GuiBackgroundWidget(this, this.leftPos + 3, this.topPos + TOP_SECTION_HEIGHT, this.imageWidth - 6, MIDDLE_SECTION_HEIGHT, com.klikli_dev.theurgy.content.gui.GuiSprites.GUI_BACKGROUND));
+        this.root.addChild(new GuiBackgroundWidget(this, this.leftPos, this.topPos + TOP_SECTION_HEIGHT + MIDDLE_SECTION_HEIGHT, this.imageWidth, BOTTOM_SECTION_HEIGHT, com.klikli_dev.theurgy.content.gui.GuiSprites.GUI_BACKGROUND.tinted(DARK_TINT)));
     }
 }

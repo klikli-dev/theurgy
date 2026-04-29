d0e5cb1650c467e8bf4c0d60c4ec4add2d1600b6
Kli Kli <klikli@outlook.at>
2026-04-29 21:28:20 +0200

    feat: add gui sprites

---
+++ src/main/java/com/klikli_dev/theurgy/content/gui/GuiSprites.java
@@ -0,0 +1,12 @@
+package com.klikli_dev.theurgy.content.gui;
+
+import com.klikli_dev.codedefinedgui.gui.texture.GuiSprite;
+import com.klikli_dev.theurgy.Theurgy;
+
+public class GuiSprites {
+    public static final GuiSprite GUI_BACKGROUND = new GuiSprite(Theurgy.loc("core/gui_background"), 16, 16);
+
+    public static final GuiSprite BUTTON = new GuiSprite(Theurgy.loc("widget/button"), 16, 16);
+    public static final GuiSprite BUTTON_DOWN = new GuiSprite(Theurgy.loc("widget/button_down"), 16, 16);
+    public static final GuiSprite BUTTON_HOVER = new GuiSprite(Theurgy.loc("widget/button_hover"), 16, 16);
+}

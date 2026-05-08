b528af6682f3fe2fd0ce6602501537dd187a8c6f
Kli Kli <klikli@outlook.at>
2026-05-08 18:39:44 +0200

    chore: remove unused widgets

--- src/main/java/com/klikli_dev/theurgy/content/gui/GuiTextures.java
+++ src/main/java/com/klikli_dev/theurgy/content/gui/GuiTextures.java
@@ -23,17 +23,7 @@ public enum GuiTextures implements ScreenElement {
     JEI_ARROW_RIGHT_FULL("jei/recipe_gui", 22, 26, 22, 16),
 
     MODONOMICON_ARROW_RIGHT(Identifier.fromNamespaceAndPath("modonomicon", "textures/gui/crafting_textures.png"), 71, 205, 9, 9, 128, 256),
-    MODONOMICON_SLOT(Identifier.fromNamespaceAndPath("modonomicon", "textures/gui/crafting_textures.png"), 84, 198, 22, 22, 128, 256),
-
-    BUTTON("widgets", 18, 18),
-    BUTTON_HOVER("widgets", 18, 0, 18, 18),
-    BUTTON_DOWN("widgets", 36, 0, 18, 18),
-
-    INDICATOR("widgets", 0, 18, 18, 6),
-    INDICATOR_WHITE("widgets", 18, 18, 18, 6),
-    INDICATOR_GREEN("widgets", 36, 18, 18, 6),
-    INDICATOR_YELLOW("widgets", 54, 18, 18, 6),
-    INDICATOR_RED("widgets", 72, 18, 18, 6);
+    MODONOMICON_SLOT(Identifier.fromNamespaceAndPath("modonomicon", "textures/gui/crafting_textures.png"), 84, 198, 22, 22, 128, 256);
 
     public final Identifier location;
     public final int textureHeight;

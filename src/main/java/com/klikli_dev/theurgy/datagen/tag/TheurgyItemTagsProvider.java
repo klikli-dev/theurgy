8cb5b626c8045d7a5a268ea4682595bc3ba1afbd
Kli Kli <klikli@outlook.at>
2026-07-30 14:37:45 +0200

    fix(tags): add missing Block import in TheurgyItemTagsProvider

--- src/main/java/com/klikli_dev/theurgy/datagen/tag/TheurgyItemTagsProvider.java
+++ src/main/java/com/klikli_dev/theurgy/datagen/tag/TheurgyItemTagsProvider.java
@@ -18,6 +18,7 @@ import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.Tags;
 
 import java.util.concurrent.CompletableFuture;
